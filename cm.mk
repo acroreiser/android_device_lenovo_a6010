5c18a0f6096b52aa5528877ee8da186e0b6b6eb1
Scott Mertz <scott@cyngn.com>
2015-01-14 17:37:25 -0800

    lettuce: Initial commit

---
+++ cm.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/yu/lettuce/full_lettuce.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_lettuce
+BOARD_VENDOR := yu
+PRODUCT_DEVICE := lettuce
+
+PRODUCT_GMS_CLIENTID_BASE := android-micromax
+
+TARGET_VENDOR_PRODUCT_NAME := LETTUCE
+TARGET_VENDOR_DEVICE_NAME := LETTUCE
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=LETTUCE PRODUCT_NAME=LETTUCE
+
+ifeq ($(SIGN_BUILD),true)
+# Signed builds gets a special boot animation because they are special.
+PRODUCT_BOOTANIMATION := device/yu/lettuce/bootanimation.zip
+endif
