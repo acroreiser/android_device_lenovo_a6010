e024a99435f6d96447d297d2dd392159a82b5388
Scott Mertz <scott@cyngn.com>
2015-03-16 13:52:15 -0700

    lettuce: 64bit support
    Change-Id: I378a9cbff78fc37d0183ef3033a7b95f89c2af5b

--- full_lettuce.mk
+++ full_lettuce.mk
@@ -15,6 +15,9 @@
 #
 
 # Inherit from those products. Most specific first.
+ifneq ($(LETTUCE_32_BIT),true)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from lettuce device
