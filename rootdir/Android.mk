8cef15eef8b218d1166925d1f3786ed53ed08725
Chao Chen <cchen@cyngn.com>
2015-03-22 23:22:19 -0700

    Lettuce: Disable diag in user build normal boot mode.
    The diag is reenabled in kernel, we need to delete the
    /dev/diag node to pass CTS.
    
    Change-Id: I035d6127580e637a3d588274a52c7a04bd908de0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -71,3 +71,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.lettuce.diag.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.lettuce.diag.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
