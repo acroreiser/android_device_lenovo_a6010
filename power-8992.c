90608e22643554cf0f11ddbc9b5c52b3e2fd0078
Dilip Gudlur <dgudlur@codeaurora.org>
2015-06-30 12:14:36 -0700

    power: Enable 4+0 core config for display off cases
    This change enables 4+0 core config for display off
    sceanrios and in display off scenario system should
    go back to default core config.
    
    Change-Id: I57795988e7e0f1b2c1e160e2e454549a14bbde50

--- power-8992.c
+++ power-8992.c
@@ -115,7 +115,6 @@ int power_hint_override(power_hint_t hint, void *data)
 
 int set_interactive_override(int on)
 {
-    return HINT_HANDLED; /* Don't excecute this code path, not in use */
     char governor[80];
 
     if (get_scaling_governor(governor, sizeof(governor)) == -1) {
@@ -128,7 +127,7 @@ int set_interactive_override(int on)
         /* Display off */
         if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
             (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
-            int resource_values[] = {}; /* dummy node */
+            int resource_values[] = {0x777}; /* 4+0 core config in display off */
             if (!display_hint_sent) {
                 perform_hint_action(DISPLAY_STATE_HINT_ID,
                 resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
