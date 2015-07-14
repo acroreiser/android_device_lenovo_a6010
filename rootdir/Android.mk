35894761ebd5b7d740e156a34d993d98601e7606
Ethan Chen <intervigil@gmail.com>
2015-07-14 15:31:41 -0700

    msm8916: Move ueventd back to common
    * It should be safe to declare device-specific nodes here.
    
    Change-Id: I3ab31dcc1ba0d216908a8cffebfb37e4123fa57d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -41,16 +41,13 @@ LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-# Devices usually have device-specific nodes, this stays on
-# device, but the file remains here for reference. Create
-# a similar device-specific block in its repo
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := ueventd.qcom.rc
-#LOCAL_MODULE_TAGS  := optional eng
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
