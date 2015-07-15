0c7fbae364ff45cbc9ecfa7876c675a94277cc8a
Ethan Chen <intervigil@gmail.com>
2015-07-14 17:24:41 -0700

    msm8196: Remove deprecated flag
    Change-Id: I36c538c795438cad0fd6fb73863509791ee17069

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,6 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
 endif
 
