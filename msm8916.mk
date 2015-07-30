1f89f0b14aa55fed969e1a559208f341eab3f73e
Ricardo Cerqueira <ricardo@cyngn.com>
2015-07-30 16:39:52 +0100

    Revert "Revert "media: Switch to lower-end codecs""
    This reverts commit 5d2b408bbe19d70483934b6dccce7c350e26fc5f.
    Here we go again... :/ We're not supposed to announce codecs
    we don't fully support (HEVC 1080p, I'm looking at you)
    
    Change-Id: If85a62b76dc5c91ac6f06f92a6fbbc814da76879

--- msm8916.mk
+++ msm8916.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_PACKAGES += \
