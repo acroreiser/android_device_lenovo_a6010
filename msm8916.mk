d8126cc963cf00f0a68d639827984320aa70d28a
Ethan Chen <intervigil@gmail.com>
2015-09-11 11:54:42 -0700

    Revert "msm8916: Add opt-out of common WCNSS configuration"
    This reverts commit b73e6c075f08d6cc55662f64ba42dea548c6d884.
    
    Change-Id: I757f3a08f4bb13397fbaa95b3b1bf8f410b614d1

--- msm8916.mk
+++ msm8916.mk
@@ -190,12 +190,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
-ifneq ($(TARGET_PROVIDES_WCNSS_CONFIG),true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-endif
 
 # WiFi Display
 ifneq ($(QCPATH),)
