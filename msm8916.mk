5efde5664c9a49906843e31befbbccb2b2729ffd
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-11-08 10:04:44 +0700

    msm8916: Enable MIDI support
    Add the required USB compositions and declare feature support.
    
    Change-Id: I64cb845042f12e4103303e38b9cdc530041bb959

--- msm8916.mk
+++ msm8916.mk
@@ -32,6 +32,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Audio
