15567ecdb644c644023dfb2ae96af1f88e5faa7c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-10 20:31:47 +0800

    msm8916: init: Check boot reason prop when setting alarm_boot prop
    This is missed out from init.qcom.early_boot.sh.
    
    Change-Id: Iab5b0d7782a53b97315d2aa9d0c301ed7725e01d
    (cherry picked from commit 013cd47bc8eb1c2c245c606d4990e33ff3a170fd)

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -73,6 +73,9 @@ static void init_alarm_boot_properties()
 {
     char const *alarm_file = "/proc/sys/kernel/boot_reason";
     char buf[64];
+    char tmp[PROP_VALUE_MAX];
+
+    property_get("ro.boot.alarmboot", tmp);
 
     if (read_file2(alarm_file, buf, sizeof(buf))) {
         /*
@@ -90,7 +93,7 @@ static void init_alarm_boot_properties()
          * 7 -> CBLPWR_N pin toggled (for external power supply)
          * 8 -> KPDPWR_N pin toggled (power key pressed)
          */
-        if (buf[0] == '3')
+        if (buf[0] == '3' || !strcmp(tmp, "true"))
             property_set("ro.alarm_boot", "true");
         else
             property_set("ro.alarm_boot", "false");
