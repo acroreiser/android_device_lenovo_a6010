f9192df888f81faf63798be5ee52bdd0222659ba
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-15 18:44:49 +0800

    msm8916: Remove duplicate aggressive TRIM prop
    This sets the default.
    
    Change-Id: Ia9b78a5db7ce4e0d0f21b257963cdf5aba16a168

--- product/perf.mk
+++ product/perf.mk
@@ -6,5 +6,4 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
-    ro.config.max_starting_bg=8 \
-    ro.sys.fw.use_trim_settings=true
+    ro.config.max_starting_bg=8
