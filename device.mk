7ced13b01152941d7ac4d9911ce49417fcc58493
dev-harsh1998 <harshitjain6751@gmail.com>
2017-02-08 15:38:18 +0530

    a6000: Ship Prebuilt GPS-hal for now

--- device.mk
+++ device.mk
@@ -145,6 +145,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.bluetooth.pbap=true \
     ro.qualcomm.bt.hci_transport=smd
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
