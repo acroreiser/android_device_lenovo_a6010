330c8a8f98b15c28f18a941c04c5b8780a04a7f5
dev-harsh1998 <harshitjain6751@gmail.com>
2017-02-09 11:09:16 +0530

    a6000: Board: add kernel information

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,18 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+# Kernel
+
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8916
+TARGET_KERNEL_CONFIG := lineageos-wt86518_defconfig
+
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
