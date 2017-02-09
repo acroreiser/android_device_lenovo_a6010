04721d32e70c985a863bad8350b06a9c373c9685
Zhao Wei Liew <zhaoweiliew@lineageos.org>
2017-02-09 19:53:05 +0530

    a6000: Android: fix firmware symlinks
     * This Fixes the baseband mount problem

--- Android.mk
+++ Android.mk
@@ -5,23 +5,24 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter a6000,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE), a6000)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
-CMN_IMAGES := \
+
+# Create a link for the WCNSS config file, which ends up as a writable
+#CMN_IMAGES := \
     cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.mdt
 
 CMN_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(CMN_IMAGES)))
@@ -116,4 +117,3 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 endif
-
