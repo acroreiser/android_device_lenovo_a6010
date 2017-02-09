c7f46cc6843ff1b9c77c56f336379689ba398271
dev-harsh1998 <harshitjain6751@gmail.com>
2017-02-09 20:31:19 +0530

    a6000: copy data files unconditionally

--- device.mk
+++ device.mk
@@ -288,14 +288,12 @@ PRODUCT_PACKAGES += \
 endif
 
 # RIL
-ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
     $(LOCAL_PATH)/configs/data/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml \
     $(LOCAL_PATH)/configs/data/dpm.conf:system/etc/dpm/dpm.conf
-endif
 
 # Keylayout
 PRODUCT_COPY_FILES += \
