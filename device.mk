863fc1640ccaf7a2dd8145e873188f7c702cb456
mshoaib <mshoaib9192@gmail.com>
2017-02-10 16:47:01 +0530

    a6000 rild: unbreak data services

--- device.mk
+++ device.mk
@@ -257,6 +257,7 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
+    persist.data.target=dpm1 \
     persist.radio.apm_sim_not_pwdn=1 \
     ro.telephony.call_ring.multiple=false \
     ro.use_data_netmgrd=true \
