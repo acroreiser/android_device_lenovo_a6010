6f75cb49dfd129c22fc413c26b1d38f26c398013
dev-harsh1998 <harshitjain6751@gmail.com>
2017-02-14 17:32:50 +0530

    a6000: netmgrd: Clean up pathings

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,6 +16,14 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.lineout.sh
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := netmgrd_control.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/netmgrd_control.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
