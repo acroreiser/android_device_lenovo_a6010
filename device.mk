88c036416781faeca5a093c43c1f9373b7dfa285
dev-harsh1998 <harshitjain6751@gmail.com>
2017-02-20 12:24:00 +0530

    a600: add missing camera permissions
    * Missed them nvm they are added now

--- device.mk
+++ device.mk
@@ -129,6 +129,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
