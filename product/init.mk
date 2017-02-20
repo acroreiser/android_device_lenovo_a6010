884a19ba6eed71315627784b33812c48c7fd9d6d
Louis Popi <theh2o64@gmail.com>
2017-02-20 09:47:23 +0100

    msm8916-common: Use CAF Memory configuration script
    Change-Id: I9f38d9176c646f7951203f626b82dfbd251ed3b9

--- product/init.mk
+++ product/init.mk
@@ -1,6 +1,7 @@
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
+    init.qcom.mem.sh \
     init.qcom.power.rc \
     init.qcom.ssr.rc \
     init.recovery.qcom.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,6 +9,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.mem.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.mem.sh
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
