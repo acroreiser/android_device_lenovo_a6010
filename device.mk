08c5dfe0880246b7dbfbf69b4ccbe8c9270c8bb3
dev-harsh1998 <harshitjain6751@gmail.com>
2017-03-11 20:08:52 +0530

    a6000: Update usb prop

--- device.mk
+++ device.mk
@@ -376,6 +376,18 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc
 
+# USB ID
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.vid=2717 \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=ff60 \
+    ro.usb.id.mtp_adb=ff68 \
+    ro.usb.id.ptp=ff10 \
+    ro.usb.id.ptp_adb=ff18 \
+    ro.usb.id.ums=ff20 \
+    ro.usb.id.ums_adb=ff28
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
