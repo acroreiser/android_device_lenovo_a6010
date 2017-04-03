69636ea049d1b77fdd5a4d9bdabd366145d21a8e
Iqbal <muhammadiqbal20122012@gmail.com>
2017-04-03 17:54:43 +0200

    a6000: init: compile as an static extention * Init Cleaned up (Removed In common Lines) * Now we compile init as a static extention to libinit_msm8916

--- init/init_a6000.cpp
+++ init/init_a6000.cpp
@@ -44,184 +44,146 @@
 #include "log.h"
 #include "util.h"
 
- #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
  
- #define ALPHABET_LEN 256
- #define KB 1024
+#define ALPHABET_LEN 256
+#define KB 1024
  
- #define IMG_PART_PATH "/dev/block/bootdevice/by-name/modem"
- #define IMG_VER_STR "QC_IMAGE_VERSION_STRING="
- #define IMG_VER_STR_LEN 24
- #define IMG_VER_BUF_LEN 255
- #define IMG_SZ 32000 * KB    /* MMAP 32000K of modem, modem partition is 64000K */
+#define IMG_PART_PATH "/dev/block/bootdevice/by-name/modem"
+#define IMG_VER_STR "QC_IMAGE_VERSION_STRING="
+#define IMG_VER_STR_LEN 24
+#define IMG_VER_BUF_LEN 255
+#define IMG_SZ 32000 * KB    /* MMAP 32000K of modem, modem partition is 64000K */
 
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-static void init_alarm_boot_properties()
-{
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
-    std::string tmp = property_get("ro.boot.alarmboot");
-
-    if (read_file2(alarm_file, buf, sizeof(buf))) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-        if (buf[0] == '3' || tmp == "true")
-            property_set("ro.alarm_boot", "true");
-        else
-            property_set("ro.alarm_boot", "false");
-    }
-}
 /* Boyer-Moore string search implementation from Wikipedia */
  
- /* Return longest suffix length of suffix ending at str[p] */
- static int max_suffix_len(const char *str, size_t str_len, size_t p) {
-     uint32_t i;
+/* Return longest suffix length of suffix ending at str[p] */
+static int max_suffix_len(const char *str, size_t str_len, size_t p) {
+    uint32_t i;
  
-     for (i = 0; (str[p - i] == str[str_len - 1 - i]) && (i < p); ) {
-         i++;
-     }
+    for (i = 0; (str[p - i] == str[str_len - 1 - i]) && (i < p); ) {
+        i++;
+    }
+
+    return i;
+}
  
-     return i;
- }
+/* Generate table of distance between last character of pat and rightmost
+ * occurrence of character c in pat
+*/
+static void bm_make_delta1(int *delta1, const char *pat, size_t pat_len) {
+    uint32_t i;
+    for (i = 0; i < ALPHABET_LEN; i++) {
+        delta1[i] = pat_len;
+    }
+    for (i = 0; i < pat_len - 1; i++) {
+        uint8_t idx = (uint8_t) pat[i];
+        delta1[idx] = pat_len - 1 - i;
+    }
+}
  
- /* Generate table of distance between last character of pat and rightmost
-  * occurrence of character c in pat
-  */
- static void bm_make_delta1(int *delta1, const char *pat, size_t pat_len) {
-     uint32_t i;
-     for (i = 0; i < ALPHABET_LEN; i++) {
-         delta1[i] = pat_len;
-     }
-     for (i = 0; i < pat_len - 1; i++) {
-         uint8_t idx = (uint8_t) pat[i];
-         delta1[idx] = pat_len - 1 - i;
-     }
- }
+/* Generate table of next possible full match from mismatch at pat[p] */
+static void bm_make_delta2(int *delta2, const char *pat, size_t pat_len) {
+    int p;
+    uint32_t last_prefix = pat_len - 1;
+
+    for (p = pat_len - 1; p >= 0; p--) {
+        /* Compare whether pat[p-pat_len] is suffix of pat */
+        if (strncmp(pat + p, pat, pat_len - p) == 0) {
+            last_prefix = p + 1;
+        }
+        delta2[p] = last_prefix + (pat_len - 1 - p);
+    }
+
+    for (p = 0; p < (int) pat_len - 1; p++) {
+        /* Get longest suffix of pattern ending on character pat[p] */
+        int suf_len = max_suffix_len(pat, pat_len, p);
+        if (pat[p - suf_len] != pat[pat_len - 1 - suf_len]) {
+            delta2[pat_len - 1 - suf_len] = pat_len - 1 - p + suf_len;
+        }
+    }
+}
  
- /* Generate table of next possible full match from mismatch at pat[p] */
- static void bm_make_delta2(int *delta2, const char *pat, size_t pat_len) {
-     int p;
-     uint32_t last_prefix = pat_len - 1;
+static char * bm_search(const char *str, size_t str_len, const char *pat, size_t pat_len) {
+    int delta1[ALPHABET_LEN];
+    int delta2[pat_len];
+    int i;
  
-     for (p = pat_len - 1; p >= 0; p--) {
-         /* Compare whether pat[p-pat_len] is suffix of pat */
-         if (strncmp(pat + p, pat, pat_len - p) == 0) {
-             last_prefix = p + 1;
-         }
-         delta2[p] = last_prefix + (pat_len - 1 - p);
-     }
+    bm_make_delta1(delta1, pat, pat_len);
+    bm_make_delta2(delta2, pat, pat_len);
  
-     for (p = 0; p < (int) pat_len - 1; p++) {
-         /* Get longest suffix of pattern ending on character pat[p] */
-         int suf_len = max_suffix_len(pat, pat_len, p);
-         if (pat[p - suf_len] != pat[pat_len - 1 - suf_len]) {
-             delta2[pat_len - 1 - suf_len] = pat_len - 1 - p + suf_len;
-         }
-     }
- }
+    if (pat_len == 0) {
+        return (char *) str;
+    }
+
+    i = pat_len - 1;
+    while (i < (int) str_len) {
+        int j = pat_len - 1;
+        while (j >= 0 && (str[i] == pat[j])) {
+            i--;
+            j--;
+        }
+        if (j < 0) {
+            return (char *) (str + i + 1);
+        }
+        i += MAX(delta1[(uint8_t) str[i]], delta2[j]);
+    }
  
- static char * bm_search(const char *str, size_t str_len, const char *pat,
-         size_t pat_len) {
-     int delta1[ALPHABET_LEN];
-     int delta2[pat_len];
-     int i;
+    return NULL;
+}
  
-     bm_make_delta1(delta1, pat, pat_len);
-     bm_make_delta2(delta2, pat, pat_len);
- 
-     if (pat_len == 0) {
-         return (char *) str;
-     }
- 
-     i = pat_len - 1;
-     while (i < (int) str_len) {
-         int j = pat_len - 1;
-         while (j >= 0 && (str[i] == pat[j])) {
-             i--;
-             j--;
-         }
-         if (j < 0) {
-             return (char *) (str + i + 1);
-         }
-         i += MAX(delta1[(uint8_t) str[i]], delta2[j]);
-     }
- 
-     return NULL;
- }
- 
- static int get_img_version(char *ver_str, size_t len) {
-     int ret = 0;
-     int fd;
-     char *img_data = NULL;
-     char *offset = NULL;
- 
-     fd = open(IMG_PART_PATH, O_RDONLY);
-     if (fd < 0) {
-         ret = errno;
-         goto err_ret;
-     }
- 
-     img_data = (char *) mmap(NULL, IMG_SZ, PROT_READ, MAP_PRIVATE, fd, 0);
-     if (img_data == (char *)-1) {
-         ret = errno;
-         goto err_fd_close;
-     }
- 
-     /* Do Boyer-Moore search across IMG data */
-     offset = bm_search(img_data, IMG_SZ, IMG_VER_STR, IMG_VER_STR_LEN);
-     if (offset != NULL) {
-         strncpy(ver_str, offset + IMG_VER_STR_LEN, len);
-     } else {
-         ret = -ENOENT;
-     }
- 
-     munmap(img_data, IMG_SZ);
- err_fd_close:
-     close(fd);
- err_ret:
-     return ret;
- }
+static int get_img_version(char *ver_str, size_t len) {
+    int ret = 0;
+    int fd;
+    char *img_data = NULL;
+    char *offset = NULL;
+
+    fd = open(IMG_PART_PATH, O_RDONLY);
+    if (fd < 0) {
+        ret = errno;
+        goto err_ret;
+    }
+
+    img_data = (char *) mmap(NULL, IMG_SZ, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (img_data == (char *)-1) {
+        ret = errno;
+        goto err_fd_close;
+    }
+
+    /* Do Boyer-Moore search across IMG data */
+    offset = bm_search(img_data, IMG_SZ, IMG_VER_STR, IMG_VER_STR_LEN);
+    if (offset != NULL) {
+        strncpy(ver_str, offset + IMG_VER_STR_LEN, len);
+    } else {
+        ret = -ENOENT;
+    }
+
+    munmap(img_data, IMG_SZ);
+err_fd_close:
+    close(fd);
+err_ret:
+    return ret;
+}
 
 int is2GB()
 {
     struct sysinfo sys;
     sysinfo(&sys);
     return sys.totalram > 1024ull * 1024 * 1024;
+}
+
+void init_target_properties()
+{
+    char modem_version[IMG_VER_BUF_LEN];
+    int rc;
+    std::string product = property_get("ro.product.name");
+    if ((strstr(product.c_str(), "a6000") == NULL))
+    return;
+
+    rc = get_img_version(modem_version, IMG_VER_BUF_LEN);
+    if (!rc) {
+        property_set("gsm.version.baseband", modem_version);
+        ERROR("Detected modem version=%s\n", modem_version);
 }
 
     /*A6000 Plus*/
@@ -239,19 +201,6 @@ int is2GB()
         property_set("ro.product.name", "Kraft-A6000");
     }
 
-void init_target_properties()
- {
-     char modem_version[IMG_VER_BUF_LEN];
-     int rc;
-    std::string product = property_get("ro.product.name");
-    if ((strstr(product.c_str(), "a6000") == NULL))
-     return;
-    rc = get_img_version(modem_version, IMG_VER_BUF_LEN);
-    if (!rc) {
-        property_set("gsm.version.baseband", modem_version);
-        ERROR("Detected modem version=%s\n", modem_version);
-}
-
     property_set("dalvik.vm.heapstartsize", "8m");
     property_set("dalvik.vm.heapgrowthlimit", is2GB() ? "192m" : "96m");
     property_set("dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
@@ -260,8 +209,4 @@ void init_target_properties()
     property_set("dalvik.vm.heapmaxfree", "8m");
 
 }
-void vendor_load_properties()
-{
-    init_target_properties();
-    init_alarm_boot_properties();
-}
+
