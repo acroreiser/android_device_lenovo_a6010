25e33fae55e8ea88c23977efe4d1414bdbe611dc
karthick111 <harshitjain6751@gmail.com>
2017-04-11 17:00:17 +0200

    a6000: add agressive trim prop

---
+++ product/perf.mk
@@ -0,0 +1,7 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=3 \
+    ro.sys.fw.use_trim_settings=true \
+    ro.sys.fw.empty_app_percent=50 \
+    ro.sys.fw.trim_empty_percent=100 \
+    ro.sys.fw.trim_cache_percent=100 \
+    ro.sys.fw.trim_enable_memory=1073741824
