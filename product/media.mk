e0c5fa3560d426d1b7c29ab66f9f07ff39e30f9a
shrukul <shrukul99@gmail.com>
2017-04-12 20:15:37 +0200

    msm8916-common: Fix wireless display
    Change-Id: I0661da3b3e141278ab4dadd5a6d81f89f7b6caf7

--- product/media.mk
+++ product/media.mk
@@ -8,3 +8,8 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true
+
+# WiFi Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+    persist.sys.wfd.virtual=0
