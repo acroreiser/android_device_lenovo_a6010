6c85e32eff793a958aeac91a0b7cc99e97898bae
dev-harsh1998 <harshitjain6751@gmail.com>
2017-05-14 20:31:15 +0530

    a6000: ship prebuilt libwcnss_qmi

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -3,6 +3,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
-
-PRODUCT_PACKAGES += \
-    libwcnss_qmi
