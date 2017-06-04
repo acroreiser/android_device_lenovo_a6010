f6e01455f606ea57fa1d1c5183831d54a30b59fe
Harshit Jain <harshitjain6751@gmail.com>
2017-06-05 00:26:37 +0530

    a6000: change flags for kernel & cpps

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,8 @@
 # Kernel
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
 TARGET_KERNEL_CONFIG := lineageos-wt86518_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
