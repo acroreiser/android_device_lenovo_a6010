5fc371dc5e910ea94fe1a8757b37a1837b4fc651
dev-harsh1998 <harshit.india@linaro.org>
2017-07-06 14:48:27 +0530

    a6000: kernel: change defconfig

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,6 +4,6 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
-TARGET_KERNEL_CONFIG := lineageos-wt86518_defconfig
+TARGET_KERNEL_CONFIG := lineageos_a6000_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
