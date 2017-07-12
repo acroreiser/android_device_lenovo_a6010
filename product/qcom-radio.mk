065cf905da412271f3af12276929906f1ca5b751
dev-harsh1998 <harshit.india@linaro.org>
2017-07-12 19:39:51 +0530

    a6000-64: Modify rild extention to 64bits

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -9,7 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.custom_ecc=1 \
     persist.radio.ecc_hard_1=112,911,110,122,119,120,000,118 \
     persist.radio.ecc_hard_count=1 \
-    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=9,1
 
