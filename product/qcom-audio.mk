49559d6fcaeee55be146d19f5f3ac4afe7678e1c
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    msm8916-common: Update audio for O
    * Build audio HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,10 +1,22 @@
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
     audio.primary.msm8916 \
-    libqcompostprocbundle \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudio-resampler \
+
+PRODUCT_PACKAGES += \
     libqcomvisualizer \
-    libqcomvoiceprocessing
+    libqcomvoiceprocessing \
+    libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
