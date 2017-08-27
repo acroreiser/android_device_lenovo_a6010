152118b8d21b2c89a849aac377699742425ef046
dev-harsh1998 <harshit.india@linaro.org>
2017-08-27 18:37:02 +0530

    a6000-64: Shift to aosp telephony

--- full_a6000.mk
+++ full_a6000.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from a6000 device
 $(call inherit-product, device/lenovo/a6000/device.mk)
