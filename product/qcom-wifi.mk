9c3db07881e41c231e321e6c9e0af030eb7b87f1
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    msm8916-common: Change WCNSS_qcom_cfg.ini file path

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -27,4 +27,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
