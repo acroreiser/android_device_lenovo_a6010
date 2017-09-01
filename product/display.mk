fd73cfa92110bc56d560aee4e5fe9907c1358d87
dev-harsh1998 <harshit.india@linaro.org>
2017-09-02 02:26:16 +0530

    a6000-64: display: set customized props

--- product/display.mk
+++ product/display.mk
@@ -13,4 +13,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+#HWUI Props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=24 \
+    ro.hwui.layer_cache_size=16 \
+    ro.hwui.r_buffer_cache_size=2 \
+    ro.hwui.path_cache_size=4 \
+    ro.hwui.gradient_cache_size=0.5 \
+    ro.hwui.drop_shadow_cache_size=2 \
+    ro.hwui.texture_cache_flushrate=0.6 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=256 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=512
