b87d2c806d1630f6a2ea718859c09823487d7235
dev-harsh1998 <harshit.india@linaro.org>
2017-09-30 20:27:58 +0530

    [DNM]a6000-64: Temp fixes

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ $(call inherit-product, device/lenovo/msm8916-common/msm8916.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
