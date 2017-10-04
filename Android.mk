2ee56e4b684332a6e83b5ff6cd76bed4bb9cade8
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-10-04 12:52:53 +0200

    lenovo_msm8916-common: Build if TARGET_VENDOR is Lenovo

--- Android.mk
+++ Android.mk
@@ -15,6 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter Lenovo,$(TARGET_VENDOR)),)
 ifneq ($(filter msm8916 msm8939,$(TARGET_BOARD_PLATFORM_VARIANT)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
@@ -115,4 +116,5 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+endif
 endif
