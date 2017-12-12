3292b1f417484160fbbe25bb36edf31d5b8af39e
dev-harsh1998 <harshit.india@linaro.org>
2017-12-13 01:33:43 +0530

    a6000-64: Resolve boot time run-out and errors
    * Overlay commit: addresses numerous PersistentDataBlockService errors during boot up.
    * This adds GMS service to persistent data partition access list
    * Perf Commit: msm8916 only has 4 cpus, we should limit dex2oat background operations to 2 thread and the associated lower memory footprint so that the impact is controlled.
    * This eases random OOM run out and heatup on boots

--- product/perf.mk
+++ product/perf.mk
@@ -1,5 +1,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=3 \
+    ro.sys.fw.dex2oat_thread_count=2 \
     ro.sys.fw.use_trim_settings=true \
     ro.sys.fw.empty_app_percent=50 \
     ro.sys.fw.trim_empty_percent=100 \
