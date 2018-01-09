73c8257bc29068bee12cab8dd9711c58e263dc54
Paras Nagda <parasnagda01@codeaurora.org>
2018-01-09 18:51:39 +0530

    a6000-64: mediacodec: Allow select and eventfd syscalls for 8916
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    
    Change-Id: Iff6e2f7fc0562a520f56cbf4bbce72f1af545f9c

---
+++ product/seccomp.mk
@@ -0,0 +1,4 @@
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
