2f39834c64e3344bb4580042b5ad849050c6b7c7
dev-harsh1998 <harshit.india@linaro.org>
2018-01-14 04:37:22 +0530

    a6000-64: products: Add hidl service and Thermal hidl hal

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,6 +1,7 @@
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     accelcal \
     AccCalibration \
     sensord \
--- product/thermal.mk
+++ product/thermal.mk
@@ -1,3 +1,8 @@
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+# Config File
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
