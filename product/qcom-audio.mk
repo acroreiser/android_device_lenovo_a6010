7ce7b76259e47ead3b1badbb2484e27e61451620
dev-harsh1998 <harshit.india@linaro.org>
2018-01-14 12:59:14 +0530

    a6000-64: audio: switch to mono audio again
    This reverts commit 109835ad595d92d4efc5580108dc3a28dd015e7f.

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -24,4 +24,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false
+    persist.audio.fluence.speaker=false \
+    persist.audio.fluence.spk.mono=true
