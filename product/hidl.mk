16cbce4c5c2c683d24ed2ffbffd5f35e543b7fdd
dev-harsh1998 <harshit.india@linaro.org>
2018-02-11 00:34:10 +0530

    a6000: add initial compatibility matrix

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,3 +1,4 @@
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
