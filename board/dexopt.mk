0932dc85aa1fb937f052c3c0cb4416d8b71e699f
dev-harsh1998 <harshit.india@linaro.org>
2018-02-10 22:56:35 +0100

    a6000: fix dexpreopt
    * Derpgang striked back then

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,8 +1,9 @@
+# Dexpreopt
 ifeq ($(HOST_OS),linux)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-        endif
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
+  endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
