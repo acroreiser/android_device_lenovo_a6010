38b20cbf8549d96422ea46726d8c3fcb1ce9fb0a
jrior001 <jriordan001@gmail.com>
2018-02-14 22:13:17 -0500

    msm8916: move wifi configs to vendor
    Change-Id: Id8b27983fa4ecf4835ae2da8b9031e9d962bdb72

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -26,6 +26,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
