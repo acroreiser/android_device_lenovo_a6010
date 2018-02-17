87f0a076a46799b11da1147c1866043d64c7f038
dev-harsh1998 <harshit.india@linaro.org>
2018-02-17 07:03:32 +0100

    [FIXME]a6000: disable media treble
    * Its breaking in app players due to OMX buffer underruns

--- product/hidl.mk
+++ product/hidl.mk
@@ -2,3 +2,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
     $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
+
+# Temp disable media treble until i fix OMX decoder for inapp players
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
