426a2f15f9f1bca15f14e86224b3b3895cc0d57c
dev-harsh1998 <harshit.india@linaro.org>
2018-02-19 22:56:30 +0530

    a6000: Bring in some optimization from caf's qcom common * Use speed profile services and wifi-service to reduce RAM and storage. * Always preopt extracted APKs to prevent extracting out of the APK for gms modules * Use a profile based boot image

--- product/perf.mk
+++ product/perf.mk
@@ -1,3 +1,9 @@
+# Optimize
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=2 \
     ro.sys.fw.use_trim_settings=true \
