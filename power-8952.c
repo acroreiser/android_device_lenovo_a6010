a36e21270f9dac63cbd1d4efba77a52b7dec7f10
Ethan Chen <intervigil@gmail.com>
2018-02-25 20:46:37 -0800

    msm8952: Support power profiles
    * Add support for power profiles
    
    Change-Id: I0ce3578530dd8618468fd97b1e49ba512cb8fa83

--- power-8952.c
+++ power-8952.c
@@ -52,68 +52,72 @@ static int saved_interactive_mode = -1;
 static int display_hint_sent;
 static int video_encode_hint_sent;
 
-static void process_video_encode_hint(void *metadata);
+static int current_power_profile = PROFILE_BALANCED;
 
-int power_hint_override(power_hint_t hint, void *data)
+static int profile_high_performance[] = {
+    CPUS_ONLINE_MIN_BIG, 0x4,
+    CPUS_ONLINE_MIN_LITTLE, 0x4,
+    CPU0_MIN_FREQ_TURBO_MAX,
+    CPU4_MIN_FREQ_TURBO_MAX,
+};
+
+static int profile_power_save[] = {
+    CPUS_ONLINE_MAX_LIMIT_BIG, 0x0,
+    CPU0_MAX_FREQ_NONTURBO_MAX,
+    CPU4_MAX_FREQ_NONTURBO_MAX,
+};
+
+static int profile_bias_power[] = {
+    CPU0_MAX_FREQ_NONTURBO_MAX,
+    CPU4_MAX_FREQ_NONTURBO_MAX,
+};
+
+static int profile_bias_performance[] = {
+    CPU0_MIN_FREQ_NONTURBO_MAX + 1,
+    CPU4_MIN_FREQ_NONTURBO_MAX + 1,
+};
+
+int get_number_of_profiles()
 {
-
-    switch (hint) {
-        case POWER_HINT_VSYNC:
-            break;
-        case POWER_HINT_VIDEO_ENCODE:
-            process_video_encode_hint(data);
-            return HINT_HANDLED;
-    }
-    return HINT_NONE;
+    return 5;
 }
 
-int set_interactive_override(int on)
+static void set_power_profile(int profile)
 {
-    char governor[80];
-    char tmp_str[NODE_MAX];
-    struct video_encode_metadata_t video_encode_metadata;
-    int rc;
+    if (profile == current_power_profile)
+        return;
 
-    ALOGI("Got set_interactive hint");
+    ALOGV("%s: Profile=%d", __func__, profile);
 
-    if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU0) == -1) {
-        if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU1) == -1) {
-            if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU2) == -1) {
-                if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU3) == -1) {
-                    ALOGE("Can't obtain scaling governor.");
-                    return HINT_HANDLED;
-                }
-            }
-        }
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: Hint undone", __func__);
     }
 
-    if (!on) {
-        /* Display off. */
-        if (is_interactive_governor(governor)) {
-            int resource_values[] = {
-                INT_OP_CLUSTER0_TIMER_RATE, BIG_LITTLE_TR_MS_50,
-                INT_OP_CLUSTER1_TIMER_RATE, BIG_LITTLE_TR_MS_50,
-                INT_OP_NOTIFY_ON_MIGRATE, 0x00
-            };
+    if (profile == PROFILE_POWER_SAVE) {
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_power_save,
+                ARRAY_SIZE(profile_power_save));
+        ALOGD("%s: Set powersave mode", __func__);
 
-            if (!display_hint_sent) {
-                perform_hint_action(DISPLAY_STATE_HINT_ID,
-                        resource_values, ARRAY_SIZE(resource_values));
-                display_hint_sent = 1;
-            }
-        } /* Perf time rate set for CORE0,CORE4 8952 target*/
-    } else {
-        /* Display on. */
-        if (is_interactive_governor(governor)) {
-            undo_hint_action(DISPLAY_STATE_HINT_ID);
-            display_hint_sent = 0;
-        }
+    } else if (profile == PROFILE_HIGH_PERFORMANCE) {
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_high_performance,
+                ARRAY_SIZE(profile_high_performance));
+        ALOGD("%s: Set performance mode", __func__);
+
+    } else if (profile == PROFILE_BIAS_POWER) {
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_bias_power,
+                ARRAY_SIZE(profile_bias_power));
+        ALOGD("%s: Set bias power mode", __func__);
+
+    } else if (profile == PROFILE_BIAS_PERFORMANCE) {
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_bias_performance,
+                ARRAY_SIZE(profile_bias_performance));
+        ALOGD("%s: Set bias perf mode", __func__);
     }
-    saved_interactive_mode = !!on;
-    return HINT_HANDLED;
+
+    current_power_profile = profile;
 }
 
-/* Video Encode Hint */
 static void process_video_encode_hint(void *metadata)
 {
     char governor[80];
@@ -136,18 +140,18 @@ static void process_video_encode_hint(void *metadata)
         }
     }
 
+    if (!metadata) {
+        return;
+    }
+
     /* Initialize encode metadata struct fields. */
     memset(&video_encode_metadata, 0, sizeof(struct video_encode_metadata_t));
     video_encode_metadata.state = -1;
     video_encode_metadata.hint_id = DEFAULT_VIDEO_ENCODE_HINT_ID;
 
-    if (metadata) {
-        if (parse_video_encode_metadata((char *)metadata,
-                    &video_encode_metadata) == -1) {
-            ALOGE("Error occurred while parsing metadata.");
-            return;
-        }
-    } else {
+    if (parse_video_encode_metadata((char *)metadata,
+                &video_encode_metadata) == -1) {
+        ALOGE("Error occurred while parsing metadata.");
         return;
     }
 
@@ -174,4 +178,75 @@ static void process_video_encode_hint(void *metadata)
             video_encode_hint_sent = 0;
         }
     }
+}
+
+int power_hint_override(power_hint_t hint, void *data)
+{
+    if (hint == POWER_HINT_SET_PROFILE) {
+        set_power_profile(*(int32_t *)data);
+        return HINT_HANDLED;
+    }
+
+    // Skip other hints in high/low power modes
+    if (current_power_profile == PROFILE_POWER_SAVE ||
+            current_power_profile == PROFILE_HIGH_PERFORMANCE) {
+        return HINT_HANDLED;
+    }
+
+    switch (hint) {
+        case POWER_HINT_VIDEO_ENCODE:
+            process_video_encode_hint(data);
+            return HINT_HANDLED;
+        default:
+            break;
+    }
+    return HINT_NONE;
+}
+
+int set_interactive_override(int on)
+{
+    char governor[80];
+    char tmp_str[NODE_MAX];
+    struct video_encode_metadata_t video_encode_metadata;
+    int rc;
+
+    ALOGI("Got set_interactive hint");
+
+    if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU0) == -1) {
+        if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU1) == -1) {
+            if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU2) == -1) {
+                if (get_scaling_governor_check_cores(governor, sizeof(governor),CPU3) == -1) {
+                    ALOGE("Can't obtain scaling governor.");
+                    return HINT_NONE;
+                }
+            }
+        }
+    }
+
+    if (!on) {
+        /* Display off. */
+        if (is_interactive_governor(governor)) {
+            int resource_values[] = {
+                INT_OP_CLUSTER0_TIMER_RATE, BIG_LITTLE_TR_MS_50,
+                INT_OP_CLUSTER1_TIMER_RATE, BIG_LITTLE_TR_MS_50,
+                INT_OP_NOTIFY_ON_MIGRATE, 0x00
+            };
+
+            if (!display_hint_sent) {
+                perform_hint_action(
+                        DISPLAY_STATE_HINT_ID,
+                        resource_values,
+                        ARRAY_SIZE(resource_values));
+                display_hint_sent = 1;
+            }
+        } /* Perf time rate set for CORE0,CORE4 8952 target*/
+    } else {
+        /* Display on. */
+        if (is_interactive_governor(governor)) {
+            undo_hint_action(DISPLAY_STATE_HINT_ID);
+            display_hint_sent = 0;
+        }
+    }
+    saved_interactive_mode = !!on;
+    return HINT_HANDLED;
 }
