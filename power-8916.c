46ac4afc550f0703f6f30674241093fa944f8c9c
dianlujitao <dianlujitao@lineageos.org>
2018-03-25 18:08:45 +0800

    power: msm8916: Add support for power profile & boost
    * Based on legacy HAL implementation
    
    Change-Id: I20225ae9244023555724c76df5e208fd60eb3841

--- power-8916.c
+++ power-8916.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -76,17 +77,151 @@ static bool is_target_8916(void)
     return is_8916;
 }
 
+static int current_power_profile = PROFILE_BALANCED;
+
+static int profile_high_performance_8916[3] = {
+    0x1C00, 0x0901, CPU0_MIN_FREQ_TURBO_MAX,
+};
+
+static int profile_high_performance_8939[11] = {
+    SCHED_BOOST_ON, 0x1C00, 0x0901,
+    CPU0_MIN_FREQ_TURBO_MAX, CPU1_MIN_FREQ_TURBO_MAX,
+    CPU2_MIN_FREQ_TURBO_MAX, CPU3_MIN_FREQ_TURBO_MAX,
+    CPU4_MIN_FREQ_TURBO_MAX, CPU5_MIN_FREQ_TURBO_MAX,
+    CPU6_MIN_FREQ_TURBO_MAX, CPU7_MIN_FREQ_TURBO_MAX,
+};
+
+static int profile_power_save_8916[1] = {
+    CPU0_MAX_FREQ_NONTURBO_MAX,
+};
+
+static int profile_power_save_8939[5] = {
+    CPUS_ONLINE_MAX_LIMIT_2,
+    CPU0_MAX_FREQ_NONTURBO_MAX, CPU1_MAX_FREQ_NONTURBO_MAX,
+    CPU2_MAX_FREQ_NONTURBO_MAX, CPU3_MAX_FREQ_NONTURBO_MAX,
+};
+
+#ifdef INTERACTION_BOOST
+int get_number_of_profiles() {
+    return 3;
+}
+#endif
+
+static void set_power_profile(int profile) {
+
+    if (profile == current_power_profile)
+        return;
+
+    ALOGV("%s: Profile=%d", __func__, profile);
+
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: Hint undone", __func__);
+    }
+
+    if (profile == PROFILE_POWER_SAVE) {
+        if (is_target_8916()) {
+            perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_power_save_8916,
+                    ARRAY_SIZE(profile_power_save_8916));
+        } else {
+            perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_power_save_8939,
+                    ARRAY_SIZE(profile_power_save_8939));
+        }
+        ALOGD("%s: Set powersave mode", __func__);
+    } else if (profile == PROFILE_HIGH_PERFORMANCE) {
+        if (is_target_8916()) {
+            perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_high_performance_8916,
+                    ARRAY_SIZE(profile_power_save_8916));
+        } else {
+            perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_high_performance_8939,
+                    ARRAY_SIZE(profile_power_save_8939));
+        }
+        ALOGD("%s: Set performance mode", __func__);
+
+    }
+
+    current_power_profile = profile;
+}
+
+static int resources_interaction_fling_boost[] = {
+    ALL_CPUS_PWR_CLPS_DIS,
+    SCHED_BOOST_ON,
+    SCHED_PREFER_IDLE_DIS,
+    0x20D
+};
+
+static int resources_interaction_boost[] = {
+    ALL_CPUS_PWR_CLPS_DIS,
+    SCHED_PREFER_IDLE_DIS,
+    0x20D
+};
+
+static int resources_launch[] = {
+    ALL_CPUS_PWR_CLPS_DIS,
+    SCHED_BOOST_ON,
+    SCHED_PREFER_IDLE_DIS,
+    0x20F,
+    0x1C00,
+    0x4001,
+    0x4101,
+    0x4201
+};
+
 int power_hint_override(power_hint_t hint, void *data)
 {
+    static struct timespec s_previous_boost_timespec;
+    struct timespec cur_boost_timespec;
+    long long elapsed_time;
+    static int s_previous_duration = 0;
+    int duration;
+
+    if (hint == POWER_HINT_SET_PROFILE) {
+        set_power_profile(*(int32_t *)data);
+        return HINT_HANDLED;
+    }
+
+    // Skip other hints in high/low power modes
+    if (current_power_profile == PROFILE_POWER_SAVE ||
+            current_power_profile == PROFILE_HIGH_PERFORMANCE) {
+        return HINT_HANDLED;
+    }
+
     switch (hint) {
-        case POWER_HINT_VSYNC:
-            break;
         case POWER_HINT_INTERACTION:
         {
-            int resources[] = {0x702, 0x20F, 0x30F};
-            int duration = 3000;
+            duration = 500; // 500ms by default
+            if (data) {
+                int input_duration = *((int*)data);
+                if (input_duration > duration) {
+                    duration = (input_duration > 5000) ? 5000 : input_duration;
+                }
+            }
 
-            interaction(duration, ARRAY_SIZE(resources), resources);
+            clock_gettime(CLOCK_MONOTONIC, &cur_boost_timespec);
+
+            elapsed_time = calc_timespan_us(s_previous_boost_timespec, cur_boost_timespec);
+            // don't hint if previous hint's duration covers this hint's duration
+            if ((s_previous_duration * 1000) > (elapsed_time + duration * 1000)) {
+                return HINT_HANDLED;
+            }
+            s_previous_boost_timespec = cur_boost_timespec;
+            s_previous_duration = duration;
+
+            if (duration >= 1500) {
+                interaction(duration, ARRAY_SIZE(resources_interaction_fling_boost),
+                        resources_interaction_fling_boost);
+            } else {
+                interaction(duration, ARRAY_SIZE(resources_interaction_boost),
+                        resources_interaction_boost);
+            }
+            return HINT_HANDLED;
+        }
+        case POWER_HINT_LAUNCH:
+        {
+            duration = 2000;
+
+            interaction(duration, ARRAY_SIZE(resources_launch),
+                    resources_launch);
             return HINT_HANDLED;
         }
         case POWER_HINT_VIDEO_ENCODE: /* Do nothing for encode case */
@@ -94,7 +229,7 @@ int power_hint_override(power_hint_t hint, void *data)
         case POWER_HINT_VIDEO_DECODE: /* Do nothing for decode case */
             return HINT_HANDLED;
         default:
-            return HINT_HANDLED;
+            break;
     }
     return HINT_NONE;
 }
