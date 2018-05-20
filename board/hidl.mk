700b18c9cfdb158ca36d464c0e7d720a96eff25f
dev-harsh1998 <harshit.india@linaro.org>
2018-05-20 14:39:22 +0530

    a6000: use make targets instead for device manifest & compability matrix

---
+++ board/hidl.mk
@@ -0,0 +1,3 @@
+# HIDL
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(LOCAL_PATH)/compatibility_matrix.xml
--- product/hidl.mk
+++
@@ -1,5 +0,0 @@
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
-    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
-
