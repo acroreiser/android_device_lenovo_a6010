4a9e62cb4dc6fafd71d2f7b7e089757ac2ad92eb
dev-harsh1998 <harshit.india@linaro.org>
2018-05-26 15:17:13 +0530

    msm8916-common: Enable media treble omx by default
    * Use the Oreo Treble mediacodec service instead
    
    Why shall 8916 miss all the love ;)?
    
    Status: Working without a glitch
    
    Follow up commits:
    
    -> device:
    dev-harsh1998/android_device_lenovo_a6000@ec33eb016391fc0eaccf54c861f69e47e01ec43e
    dev-harsh1998/android_device_lenovo_a6000@f603ea6cfb78cb338d61f3212f61edb6d848503b
    
    -> common:
    dev-harsh1998/android_device_lenovo_msm8916-common@48d0c6a1a7048269f79f0819e108e2216263f24f
    
    -> vendor:
    dev-harsh1998/proprietary-vendor_lenovo@3c6802ea3ed4954663ccbfde371a76b2b79d3700

--- product/media.mk
+++ product/media.mk
@@ -4,10 +4,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false
-
 # WiFi Display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1
