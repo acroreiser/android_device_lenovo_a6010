b4af79d2d676cd7e0fca6710f64a5dcc8716f025
Aditya <adithya.r02@outlook.com>
2018-06-09 14:59:20 +0530

    a6000: overlay: add network attribute for LTE(9) and GSM/EDGE (1)
    * They can co-exist together
    * Add the lovely flag for ancient RIL blob
    * Fix overlay derp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,7 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmmcamera2_stats_modules.so|libshim_atomic.so
 
 # Qualcomm support
+DISABLE_RILD_OEM_HOOK := false
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 MALLOC_SVELTE := true
