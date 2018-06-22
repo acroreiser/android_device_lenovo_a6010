c2666e79b3f5f89d022a7575648780b2c15fe7af
Harshit Jain <harshitjain6751@gmail.com>
2018-06-22 17:44:16 +0530

    msm8916-common: Update codec targets

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -6,8 +6,12 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libOmxVdec \
+    libOmxVdecHevc \
+    libOmxSwVencHevc \
     libOmxVenc \
+    libOmxVidcCommon \
     libstagefrighthw
+    
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml
