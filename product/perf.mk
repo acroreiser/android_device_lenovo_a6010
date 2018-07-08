995f17efdedc52acf2cddec3bc3142da70dc7a3a
dev-harsh1998 <harshitjain6751@gmail.com>
2018-07-09 00:06:38 +0530

    a6000: perf: Update and tune props based on nokia 2's build.prop
    * https://transfer.sh/86Yta/build.prop

--- product/perf.mk
+++ product/perf.mk
@@ -5,22 +5,20 @@ PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=2 \
-    ro.sys.fw.use_trim_settings=true \
-    ro.sys.fw.empty_app_percent=50 \
-    ro.sys.fw.trim_empty_percent=100 \
-    ro.sys.fw.trim_cache_percent=100 \
-    ro.sys.fw.trim_enable_memory=1073741824 \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapgrowthlimit=128m \
     dalvik.vm.heapminfree=6m \
-    dalvik.vm.heapstartsize=14m \
+    dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapsize=256m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.zygotemaxfailedboots=5 \
+    dalvik.vm.foreground-heap-growth-multiplier=2.0 \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.dex2oat-threads=2 \
+    ro.vendor.qti.am.reschedule_service=true \
     sys.use_fifo_ui=1
-    
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
