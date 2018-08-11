f710b51e6a9c989aa7c0e26b42caf7f8ccbbc66e
MkfsSion <myychina28759@gmail.com>
2018-08-11 12:43:44 +0530

    a6000: kernel: Use zImage-dtb instead of zImage
    Change-Id: Ifea3542a87ed5e5a1bc1d3b63e5ea5122952545b
    Signed-off-by: MkfsSion <myychina28759@gmail.com>
    
    * This dormatizes the use of [HEX-FIRMWARE-PATCH](https://androidfilehost.com/?fid=889764386195913793) that has patched LK to support this else devices won't boot.
    * This is to break rom booting *stuck on splash* on russian variants where users don't flash the correct firmware and report useless fw related issues.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,14 +30,12 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
-BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/gcc8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-gnueabi-
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
