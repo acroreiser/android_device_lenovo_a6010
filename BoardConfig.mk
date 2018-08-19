8cc344d86af7a3b82c67e808de3e7cded5a8519b
dev-harsh1998 <harshitjain6751@gmail.com>
2018-08-19 17:41:36 +0530

    a6000: use android's default androidkernel gcc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,8 +38,6 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/gcc8/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-gnueabi-
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
 TARGET_KERNEL_CONFIG := lineageos_a6000_defconfig
 
