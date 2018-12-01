5400ef66d94a0d091a4e09b54823392cea5fc31d
dev-harsh1998 <dev-harsh1998@hotmail.com>
2018-12-01 12:05:38 +0530

    a6000: add support for aicp build

---
+++ aicp_a6000.mk
@@ -0,0 +1,50 @@
+# Copyright (C) 2015-2017 The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, device/lenovo/a6000/device.mk)
+
+# Inherit aicp specific configuration
+$(call inherit-product, $(TOPDIR)vendor/aicp/config/common_full_phone.mk)
+
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8916
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
+
+TARGET_VENDOR := Lenovo
+PRODUCT_DEVICE := a6000
+PRODUCT_NAME := aicp_a6000
+BOARD_VENDOR := Lenovo
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := A6000
+PRODUCT_MANUFACTURER := Lenovo
+
+# Device Identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     PRODUCT_MODEL="Lenovo A6000" \
+     PRODUCT_NAME="Kraft-A6000" \
+     PRODUCT_DEVICE="Kraft-A6000"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.build.product=Kraft-A6000
+
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Kraft-A6000-user 5.0.2 LRX22G Kraft-A6000_S061_160727 release-keys"
+
+BUILD_FINGERPRINT := Lenovo/Kraft-A6000/Kraft-A6000:5.0.2/LRX22G/Kraft-A6000_S061_160727:user/release-keys
