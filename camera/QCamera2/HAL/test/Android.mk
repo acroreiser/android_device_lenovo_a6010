3a519467a39f159af6921bfc4d81495dd484f458
dev-harsh1998 <dev-harsh1998@hotmail.com>
2018-12-02 03:16:30 +0530

    Revert "a6000: camera: rip pathmap"
    This reverts commit 0dfdc2f71332a15d7d8ff008c11179a5165d7f3e.

--- camera/QCamera2/HAL/test/Android.mk
+++ camera/QCamera2/HAL/test/Android.mk
@@ -40,7 +40,7 @@ LOCAL_C_INCLUDES += \
     frameworks/base/include/media \
     external/skia/include/core \
     external/skia/include/images \
-    hardware/qcom/display-caf-msm8916/libgralloc \
+    $(call project-path-for,qcom-display)/libgralloc \
     frameworks/av/include/media/stagefright \
     frameworks/native/include/media/openmax \
 
