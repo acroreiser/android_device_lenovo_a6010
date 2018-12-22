ffd6b6a31dde60c98b6b9e048379bce882ac7fa3
Bruno Martins <bgcngm@gmail.com>
2018-12-22 18:58:43 +0000

    A6010: enable memory optimizations
     * Enable B-services aging
     propagation and delay service restart for devices with 2GB or less memory.

--- device.mk
+++ device.mk
@@ -558,6 +558,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.downgrade_pressure=60 \
     ro.lmk.kill_heaviest_task=false
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    ro.vendor.qti.sys.fw.bservice_age=5000
+
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.vendor.shutdown.waittime=500 \
