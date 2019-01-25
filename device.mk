39543f47746a7912d9024a511d6b164b9581a167
Akianonymus <anonymus.aki@gmail.com>
2019-01-25 09:26:51 +0530

    msm8916-common: Disable oem unlock
    * as stated in this commit
    	https://github.com/LineageOS/android_packages_apps_Settings/commit/c240992b4c86c7f226290807a2f41f2619e7e5e8
    
    * Log:
    	Process: com.android.settings
    	PID: 26986
    	Flags: 0x38c83e45
    	Package: com.android.settings v28 (9)
    	Foreground: Yes
    	Subject: SystemServiceRegistry
    	Build: motorola/harpia/harpia:7.1.1/NPIS26.48-36-5/12:user/release-keys
    
    	android.util.Log: No service published for: oem_lock
    	Caused by: android.os.ServiceManager: No service published for: oem_lock
    	at android.os.ServiceManager.getServiceOrThrow(ServiceManager.java:143)
    	at android.app.SystemServiceRegistry6.createService(SystemServiceRegistry.java:843)
    	at android.app.SystemServiceRegistry6.createService(SystemServiceRegistry.java:840)
    	at android.app.SystemServiceRegistry.getService(SystemServiceRegistry.java:1144)
    	... 32 more

--- device.mk
+++ device.mk
@@ -435,6 +435,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
 
+# Disable oem unlock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.oem_unlock_supported=false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
