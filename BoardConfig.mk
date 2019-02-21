7007f56ce40436a8f2ed4731e68909da15a91855
Anton Hansson <hansson@google.com>
2019-02-21 15:04:20 +0000

    msm8916-common: Set cpu "runtime variant" to cortex-a53
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the cortex-a53 variant at runtime. It does not affect
    build-time compilation.
    
    Bug: 120773446
    Test: bionic benchmarks with and without this change
    Change-Id: I670c8acd094090135515688c5f057c2cc08a083f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 TARGET_CPU_FEATURES := div,atomic_ldrd_strd
 
 # Binder
