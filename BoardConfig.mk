dfd177ea8ec456db1a60ea4e5572b84852305b5a
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    a6000: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ TARGET_USES_QCOM_WCNSS_QMI := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # Proprietary Prebuilt
 -include vendor/lenovo/a6000/BoardConfigVendor.mk
