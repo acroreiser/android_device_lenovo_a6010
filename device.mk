0620f8befde0bbccd1f07c221b188129b96c2781
Prakash Pabba <ppabba@codeaurora.org>
2019-08-06 15:27:55 +0530

    a6010: Enable ipsec_tunnel feature
    Fixed : CTS issue due to ipsec_tunnel feature.
    
    CTS issues are showing failure on devices with first_api_level set to
    29 . With this api level certain test cases which weren't executing
    till now begin executing. Ex: android.net.cts.IpSecManagerTunnelTest.
    
    These tests mandatorily requrire ipsec_tunnel feature to be enabled.
    
    Change-Id: If52b652ac3cc6488fefcf851527f224efa03e7eb

--- device.mk
+++ device.mk
@@ -465,7 +465,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
