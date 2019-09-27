d7104b0d36acc24f1d6910c3f24478f7628d1779
Akash Mondal <a98mondal@gmail.com>
2019-09-28 02:22:45 +0530

    a6010: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- device.mk
+++ device.mk
@@ -356,7 +356,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
