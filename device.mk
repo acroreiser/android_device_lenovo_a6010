a57c7c28bab5764c712a238fd9857326f34206ee
Jaroslav Zviezda <acroreiser@gmail.com>
2020-01-02 18:03:40 +0300

    Revert "a6000: build drm 1.2 clearkey service"
    This reverts commit b873412ad25099261bb385b903a804c5b7ae1c6c.

--- device.mk
+++ device.mk
@@ -74,8 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.0-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
