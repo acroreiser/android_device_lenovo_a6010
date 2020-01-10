e8d8b75f9710cb7a4c212eda4c4bcad7cdf03c9c
The-Skeptic <52853005+The-Skeptic@users.noreply.github.com>
2020-01-10 21:03:09 +0200

    Update init_msm8916.cpp
    Updated dalvik heap values for 1GB and 2GB devices.

--- baseband_init/init_msm8916.cpp
+++ baseband_init/init_msm8916.cpp
@@ -91,21 +91,23 @@ void vendor_load_properties()
 {
 
 if (is2GB()) {
-	property_set("dalvik.vm.heapstartsize", "16m");
-	property_set("dalvik.vm.heapgrowthlimit", "256m");
-	property_set("dalvik.vm.heapsize", "512m");
+	property_set("dalvik.vm.heapstartsize", "8m");
+	property_set("dalvik.vm.heapgrowthlimit", "128m");
+	property_set("dalvik.vm.heapsize", "256m");
 	property_set("dalvik.vm.heaptargetutilization", "0.75");
 	property_set("dalvik.vm.heapminfree", "2m");
+	property_set("dalvik.vm.heapmaxfree", "8m");
 
 // Cached apps limit
   property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "17");
 }
     else {
 	property_set("dalvik.vm.heapstartsize", "8m");
-	property_set("dalvik.vm.heapgrowthlimit", "128m");
-	property_set("dalvik.vm.heapsize", "256m");
+	property_set("dalvik.vm.heapgrowthlimit", "64m");
+	property_set("dalvik.vm.heapsize", "128m");
 	property_set("dalvik.vm.heaptargetutilization", "0.75");
-	property_set("dalvik.vm.heapminfree", "512k");
+	property_set("dalvik.vm.heapminfree", "2m");
+	property_set("dalvik.vm.heapmaxfree", "8m");
 
 // Cached apps limit
   property_set("ro.vendor.qti.sys.fw.bg_apps_limit", "9");
