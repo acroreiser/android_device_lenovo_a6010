d075385f85f4444958b7d4d4a5dfb48b14e2bae7
nichcream <c.potato23@gmail.com>
2020-01-10 17:07:54 +0530

    santoni: camera: HACK: Hardcode preview fps range
     * If userspace does not specify a range, it would be defaulted to
       30000,30000 (30-30fps), which causes really bad exposure under
       low light during video recording in certain apps (SnapdragonCamera
       for one *cough cough*). We definitly do not want this.
     * Work around this issue by always forcing min fps of 7.
    
    @acroreiser: adapted for a6010

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -5339,8 +5339,9 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
       min_fps = max_fps = vid_min_fps = vid_max_fps = (int)fixedFpsValue*1000;
     }
     snprintf(str, sizeof(str), "%d,%d", min_fps, max_fps);
-    CDBG("%s: Setting preview fps range %s", __func__, str);
-    updateParamEntry(KEY_PREVIEW_FPS_RANGE, str);
+    CDBG("Actual preview fps range %s", str);
+    updateParamEntry(KEY_PREVIEW_FPS_RANGE, "7000,30000");
+    CDBG("Setting the preview fps range 7000,30000");
     cam_fps_range_t fps_range;
     memset(&fps_range, 0x00, sizeof(cam_fps_range_t));
     fps_range.min_fps = (float)min_fps / 1000.0f;
