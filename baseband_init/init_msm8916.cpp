3e0a87071aa5885e5db022ab6bb52ce57465e24c
Jaroslav Zviezda <acroreiser@gmail.com>
2020-01-12 20:03:47 +0300

    init: enable Project Svelte for 1/8

--- baseband_init/init_msm8916.cpp
+++ baseband_init/init_msm8916.cpp
@@ -92,6 +92,7 @@ void vendor_load_properties()
 
   if (is2GB())
   {
+     property_set("ro.config.max_starting_bg", "8");
 	   property_set("dalvik.vm.heapstartsize", "8m");
 	   property_set("dalvik.vm.heapgrowthlimit", "128m");
 	   property_set("dalvik.vm.heapsize", "256m");
@@ -104,7 +105,10 @@ void vendor_load_properties()
   }
   else
   {
-	   property_set("dalvik.vm.heapstartsize", "8m");
+     property_set("ro.config.low_ram", "true");
+     property_set("persist.sys.force_highendgfx", "true");
+     property_set("ro.config.max_starting_bg", "6");
+	   property_set("dalvik.vm.heapstartsize", "5m");
 	   property_set("dalvik.vm.heapgrowthlimit", "64m");
 	   property_set("dalvik.vm.heapsize", "128m");
 	   property_set("dalvik.vm.heaptargetutilization", "0.75");
