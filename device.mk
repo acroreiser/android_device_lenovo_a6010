31bd9edc96f30643a743517b8837691940f88e4f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-01-15 09:24:26 +0530

    A6010: Add privapp permission for Snap
    from https://github.com/LineageOS/android_vendor_lineage/blob/lineage-17.1/config/permissions/privapp-permissions-lineage-system.xml

--- device.mk
+++ device.mk
@@ -225,7 +225,8 @@ PRODUCT_COPY_FILES += \
 
 # PrivApp Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
