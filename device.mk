50aeef2c7ecae93adc824b6a0fdbbe432e0eb2ad
Jaroslav Zviezda <acroreiser@gmail.com>
2020-02-05 01:23:35 +0300

    make overlay-lineage happy!

--- device.mk
+++ device.mk
@@ -222,11 +222,15 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Enforced RRO targets
  PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
