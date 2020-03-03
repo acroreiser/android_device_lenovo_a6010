8341de03a95880217d09040bc3fad4d66a0be52e
Jaroslav Zviezda <acroreiser@gmail.com>
2020-03-04 01:22:06 +0300

    a6010: enable support for USB host at ROM side

--- device.mk
+++ device.mk
@@ -357,6 +357,7 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
 
 # Vibrator
