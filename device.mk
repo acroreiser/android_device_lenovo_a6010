5db10c67b00bb206e08ddaaa227afad6f940cec4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-25 21:20:55 +0100

    msm8916-common: build Soundtrigger HIDL

--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
     android.hardware.audio@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl
 
