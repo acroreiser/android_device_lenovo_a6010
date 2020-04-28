5c1e0da14a2a20ea1f26d6007852174dff77faec
Jaroslav Zviezda <acroreiser@gmail.com>
2020-04-28 23:20:34 +0300

    A6010(crdroid): switch to external screen recorder

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
     libshim_atomic \
     Snap
 
+# Screen Recorder
+PRODUCT_PACKAGES += \
+    Recorder
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
