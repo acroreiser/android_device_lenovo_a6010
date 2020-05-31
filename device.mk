f1d3e919c2c4530125892e7548cc0cb0b0c72f2c
Jaroslav Zviezda <acroreiser@gmail.com>
2020-05-31 19:51:16 +0300

    [DNM] A6010: fix up GLONASS
    Switch to android.hardware.gnss@1.0-{impl|service}.legacy
    
    Requires next commits to hardware_lineage_interfaces:
    https://review.lineageos.org/c/LineageOS/android_hardware_lineage_interfaces/+/276313/2
    https://review.lineageos.org/c/LineageOS/android_hardware_lineage_interfaces/+/276314/2
    https://review.lineageos.org/c/LineageOS/android_hardware_lineage_interfaces/+/276315/3

--- device.mk
+++ device.mk
@@ -220,8 +220,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
+    android.hardware.gnss@1.0-impl.legacy \
+    android.hardware.gnss@1.0-service.legacy \
     gps.msm8916
 
 # Charger images
