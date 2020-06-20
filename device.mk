cfccfab8276fb217744ecb9ea252a6b71bd4549d
Jaroslav Zviezda <acroreiser@gmail.com>
2020-06-20 19:36:34 +0300

    a6010: binderize camera provider

--- device.mk
+++ device.mk
@@ -200,6 +200,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.msm8916 \
     libmm-qcamera \
