f8ac72385f4ac429ae1fda99d48591262da5e489
Jaroslav Zviezda <acroreiser@gmail.com>
2020-06-20 17:51:52 +0300

    a6010: add thermal-engine
    Also reduce throttling and shutdown thresholds.

--- device.mk
+++ device.mk
@@ -608,6 +608,11 @@ PRODUCT_PACKAGES += \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
+# Thermal
+PRODUCT_PACKAGES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Sensors
 PRODUCT_PACKAGES += \
     calmodule.cfg \
