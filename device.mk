8a931d5e8cd350bb38376bd4888c2f37807255c1
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-06-23 18:32:11 +0530

    a6010: Up-rev Health hal from 2.0 to 2.1.
    Change-Id: Ib2d1e7742b49919045022d385460e0a3fedf3033

--- device.mk
+++ device.mk
@@ -415,8 +415,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
