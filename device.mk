e8baa015dcf4af5023aa5c01fb2f758ec2f4cf7c
Jaroslav Zviezda <acroreiser@gmail.com>
2020-06-24 22:51:06 +0300

    a6010: import Pixel Thermal HAL 2.0
    * rename to -msm8916
    * config for msm8916 sensors
    
    From: https://android.googlesource.com/platform/hardware/google/pixel/+/refs/heads/master/thermal

--- device.mk
+++ device.mk
@@ -615,7 +615,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
-# Thermal
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.msm8916
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.thermal.config=thermal_info_config.json
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
