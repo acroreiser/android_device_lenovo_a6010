7e0b082b6754605ee52ca01f414b92b3f3a14c6d
Jaroslav Zviezda <acroreiser@gmail.com>
2020-07-23 19:00:47 +0300

    [go] a6010: add memory trim properties

--- device.mk
+++ device.mk
@@ -564,7 +564,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.bservice_enable=true \
     ro.vendor.qti.sys.fw.bservice_limit=5 \
-    ro.vendor.qti.sys.fw.bservice_age=5000
+    ro.vendor.qti.sys.fw.bservice_age=5000 \
+    ro.vendor.qti.sys.fw.use_trim_settings=true \
+    ro.vendor.qti.sys.fw.empty_app_percent=50 \
+    ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+    ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+
 
 # Boot
 PRODUCT_PROPERTY_OVERRIDES += \
