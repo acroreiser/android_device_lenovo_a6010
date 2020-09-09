4f9819a7f560e50a504b3150d92e68cdb1cb45c3
Herobuxx <herobuxx@gmail.com>
2020-09-10 01:11:55 +0700

    a6010: build hidl base and hidl manager for both system and vendor

--- device.mk
+++ device.mk
@@ -388,7 +388,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # Binder
 PRODUCT_PACKAGES += \
