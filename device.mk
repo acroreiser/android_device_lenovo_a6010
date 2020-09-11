0208a8c3d81681abc9979a1c1f40a8093b0a2251
Jaroslav Zviezda <acroreiser@gmail.com>
2020-09-11 19:41:27 +0300

    a6010: bump camera.provider to 2.5

--- device.mk
+++ device.mk
@@ -197,8 +197,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5-impl \
+    android.hardware.camera.provider@2.5-service \
     camera.device@1.0-impl \
     camera.msm8916 \
     libmm-qcamera \
