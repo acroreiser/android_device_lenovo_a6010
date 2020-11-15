0c47b8c4f4750e2295b0e5e8cd575604368c0365
Jaroslav Zviezda <acroreiser@gmail.com>
2020-11-15 15:27:34 +0300

    a6010: fix baseband version detection

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,4 +1,4 @@
 #! /vendor/bin/sh
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /firmware/image/modem.b17 | grep "S86528C1" | head -1`
+setprop gsm.version.baseband `strings /firmware/image/modem.b17 | grep "S86528C" | head -1`
