268de9318db54bd2fb6cbfac631f1409d01359a4
Vijay Venkatraman <vijaykv@google.com>
2020-11-20 13:31:46 +0000

    msm8916-common: gps: Replacing copy headers with header  libraries
    Bug: 33241851
    Test: Build target and pass CTS location tests
    Change-Id: I75db5e6de3c4194a6a46e0d37fcd899ea1d1db04

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,4 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
 #Compile this library only for builds with the latest modem image
 
 LOCAL_PATH := $(call my-dir)
@@ -49,25 +48,6 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/platform_lib_abstractions
 
-LOCAL_COPY_HEADERS_TO:= gps.utils/
-LOCAL_COPY_HEADERS:= \
-   loc_log.h \
-   loc_cfg.h \
-   log_util.h \
-   linked_list.h \
-   msg_q.h \
-   MsgTask.h \
-   LocHeap.h \
-   LocThread.h \
-   LocTimer.h \
-   loc_target.h \
-   loc_timer.h \
-   LocSharedLock.h \
-   platform_lib_abstractions/platform_lib_includes.h \
-   platform_lib_abstractions/platform_lib_time.h \
-   platform_lib_abstractions/platform_lib_macros.h \
-   loc_misc_utils.h
-
 LOCAL_MODULE := libgps.utils
 LOCAL_MODULE_OWNER := qcom
 LOCAL_VENDOR_MODULE := true
@@ -77,4 +57,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgps.utils_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH) $(LOCAL_PATH)/platform_lib_abstractions
+include $(BUILD_HEADER_LIBRARY)
