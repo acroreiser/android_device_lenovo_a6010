35fbb681692fd57ce779c2377da5eedcd8398d19
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-17 22:29:22 +0530

    a6010: Enable QCRIL radio power saving

--- device.mk
+++ device.mk
@@ -417,7 +417,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.add_power_save=0 \
+    persist.radio.add_power_save=1 \
     ro.telephony.call_ring.multiple=false
 
 # HIDL
