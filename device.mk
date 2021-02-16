b4f895d228612d0171d22bd3fe040278d2d4c40c
Yaroslav Zviezda <acroreiser@gmail.com>
2021-02-17 01:55:10 +0300

    a6010: limit background apps to 7

--- device.mk
+++ device.mk
@@ -582,7 +582,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.empty_app_percent=50 \
     ro.vendor.qti.sys.fw.trim_empty_percent=100 \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=7
 
 
 # Boot
