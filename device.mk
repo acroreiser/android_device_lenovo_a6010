2796c314958ffe0cf96496e3de14e599e93c1dc1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-02 00:55:58 +0100

    a6010: Use default keymaster@4.1 HIDL
    * We use software keymaster anyway, let's avoid letting keymaster HIDL down at every boot seeing that a secure keystore doesn't exists
    
    Change-Id: Ic597f98dfec91e476e08a0b9fc7d9a59da7084e1

--- device.mk
+++ device.mk
@@ -284,8 +284,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@4.1-service \
     android.hardware.authsecret@1.0-service \
     android.hardware.gatekeeper@1.0-service.software
 
