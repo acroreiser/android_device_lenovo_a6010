e80470c2b26374f82bc7d7583d6979d02de35566
Yaroslav Zviezda <acroreiser@gmail.com>
2021-03-14 00:34:44 +0300

    a6010: tune and reorganize memory configs

--- rootdir/etc/set_zram.sh
+++ rootdir/etc/set_zram.sh
@@ -1,15 +1,18 @@
 #! /vendor/bin/sh
 
-# Setup ZRAM
-swapoff /dev/block/zram0
-sleep 0.5
 ZMEM=$(cat /proc/meminfo | grep MemTotal | awk  '{print $2}')
 
+# Reduce io thrashing
 if [ "$ZMEM" -gt "1100000" ]; then
-   echo "50" > /proc/sys/vm/vmpressure_level_med
+	echo "50" > /proc/sys/vm/vmpressure_level_med
+	echo 81920 > /proc/sys/vm/min_free_kbytes
+	echo 81920 > /proc/sys/vm/extra_free_kbytes
+else
+	echo 40960 > /proc/sys/vm/min_free_kbytes
+	echo 40960 > /proc/sys/vm/extra_free_kbytes
 fi
 
-
+# Setup ZRAM to 75% of memtotal
 let 'ZMEM=((ZMEM/100)*75)*1024'
 echo 1 > /sys/block/zram0/reset
 echo 'lz4' > /sys/block/zram0/comp_algorithm
@@ -19,5 +22,4 @@ sleep 0.5
 mkswap /dev/block/zram0
 sleep 0.5
 swapon /dev/block/zram0
-echo 100 > /proc/sys/vm/swappiness
-
+echo 35 > /proc/sys/vm/swappiness
