03a2515b6d9495bf71cb766eb596ba8e2c122804
Chenyang Zhong <zhongcy95@gmail.com>
2021-04-01 20:43:27 +0300

    a6010: enable Seamless Transfer
    It allows users to switch between audio output devices by tapping the music
    note icon on the notification of a music app.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.fflag.override.settings_tether_all_in_one=true
 
+# Seamless transfer
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.fflag.override.settings_seamless_transfer=true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
