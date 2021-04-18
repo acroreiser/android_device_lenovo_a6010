a2ec1f675f5f7acb75f292ace2a33ce7d38d17fa
Yaroslav Zviezda <acroreiser@gmail.com>
2021-04-18 17:04:27 +0300

    a6010: move DocumentsUI overlays to DocumentsUIOverlay

---
+++ DocumentsUIOverlay/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
+LOCAL_CERTIFICATE := platform
+LOCAL_PACKAGE_NAME := DocumentsUIOverlay
+LOCAL_SDK_VERSION := current
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/overlay
+include $(BUILD_PACKAGE)
