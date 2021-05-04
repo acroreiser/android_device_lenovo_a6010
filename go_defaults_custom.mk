6c30264399b39b33190f65b3946c05b408b477b3
Dominggoes Isakh <drjisakh@gmail.com>
2021-05-04 07:52:39 +0200

    a6010: Customize GO configuration
    * Full Go configuration
    * Enabled 512M Go configs
    * More aggressive configs from Nokia, ZTE
    * Kill heaviest app to faster RAM reclaiming
    * Disable traced
    
    Co-authored-by: Yaroslav Zviezda <acroreiser@gmail.com>

---
+++ go_defaults_custom.mk
@@ -0,0 +1,50 @@
+# Sets Android Go recommended default product options..
+
+DEVICE_PATH := device/lenovo/a6010
+
+# Set lowram options and enable traced by default
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.config.low_ram=true \
+     persist.traced.enable=false
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Always preopt extracted APKs to prevent extracting out of the APK for gms
+# modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Do not spin up a separate process for the network stack on go devices, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += CellBroadcastAppPlatform
+PRODUCT_PACKAGES += CellBroadcastServiceModulePlatform
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Disable Scudo outside of eng builds to save RAM.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_DISABLE_SCUDO := true
+endif
+
+# Add the system properties.
+TARGET_SYSTEM_PROP += \
+    $(DEVICE_PATH)/go_defaults.prop
+
+# use the go specific handheld_core_hardware.xml from frameworks
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/go_handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+# Dedupe VNDK libraries with identical core variants.
+TARGET_VNDK_USE_CORE_VARIANT := true
