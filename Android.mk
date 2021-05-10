b2b757520b63c57843e9b390d64fcac96651f75b
Arbab(AB) <arbabaliabforever@gmail.com>
2021-05-10 06:20:16 +0500

    Make Android.mk search recursive

--- Android.mk
+++ Android.mk
@@ -16,7 +16,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE), a6010)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
 include $(CLEAR_VARS)
 
 CMN_IMAGES := \
