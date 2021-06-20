a50cee0d9e27395c884a47ed9ecd0b851d0daadd
Yaroslav Zviezda <acroreiser@gmail.com>
2021-06-20 23:41:15 +0300

    a6010: set_baseband: cache modem version on first boot
    Reduces boot time for ~3s at next boot.

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,4 +1,10 @@
 #! /vendor/bin/sh
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /firmware/image/modem.b17 | grep "S86528C" | head -1`
+
+if [ -f "/data/modem_version.txt" ]; then
+    setprop gsm.version.baseband `cat /data/modem_version.txt`
+else
+    echo `strings /firmware/image/modem.b17 | grep "S86528C" | head -1` > /data/modem_version.txt
+    setprop gsm.version.baseband `cat /data/modem_version.txt`
+fi
