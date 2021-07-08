9ea78f7dbf1e3e66ade36d8d066f21ad00662fa1
Yaroslav Zviezda <acroreiser@gmail.com>
2021-07-08 23:23:04 +0300

    [go] a6010: harder background limits

--- rootdir/etc/target_config.sh
+++ rootdir/etc/target_config.sh
@@ -6,14 +6,14 @@ ZMEM=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 if [ "$ZMEM" -gt "1100000" ]; then
 	setprop persist.device_config.runtime_native.usap_pool_enabled true
 	setprop ro.vendor.qti.sys.fw.trim_enable_memory 2147483648
-	setprop ro.vendor.qti.sys.fw.bg_apps_limit 16
+	setprop ro.vendor.qti.sys.fw.bg_apps_limit 4
 	setprop dalvik.vm.usejit true
 	setprop dalvik.vm.usejitprofiles true
 	setprop pm.dexopt.install speed-profile
 else
 	setprop persist.device_config.runtime_native.usap_pool_enabled false
 	setprop ro.vendor.qti.sys.fw.trim_enable_memory 1073741824
-	setprop ro.vendor.qti.sys.fw.bg_apps_limit 4
+	setprop ro.vendor.qti.sys.fw.bg_apps_limit 2
 	setprop dalvik.vm.usejit false
 	setprop dalvik.vm.usejitprofiles false
 	setprop pm.dexopt.install speed
