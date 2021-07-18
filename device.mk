755be7fb6a20d7ccd11ffc5e5f4a9f26a4abbbbc
Yaroslav Zviezda <acroreiser@gmail.com>
2021-07-18 09:27:39 +0300

    a6010: add Gboard overlay
    * Disable Firebase and some unwanted things.
    * Enable battery saver theme switching.
    
    Based on bools.xml from https://github.com/acroreiser/gapps_android_11_go/blob/lineage-18.1/system/product/app/LatinImeGoogleGo/LatinImeGoogleGo.apk

--- device.mk
+++ device.mk
@@ -285,6 +285,10 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
+# Overlay for Gboard
+PRODUCT_PACKAGES += \
+    GboardOverlay
+
 # DocumentsUI overlay
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay
