93b29e1b13074cb3cfadfecea6651576f596b068
Yaroslav Zviezda <acroreiser@gmail.com>
2021-07-31 11:12:22 +0300

    a6010: set_baseband.sh: use QC_IMAGE_VERSION_STRING for version string and grep all modem files to find it
    * This makes set_baseband.sh universal for at least all msm8916 devices

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,10 +1,14 @@
 #! /vendor/bin/sh
 
-# grep the modem partition for baseband version and set it
+# grep the modem partition for baseband version and set and cache it
+# if modem_version.txt is present, cat it and use
+
+# use QC_IMAGE_VERSION_STRING as unified for all 8916 modem images
+# grep all modem files for first time
 
 if [ -f "/data/modem_version.txt" ]; then
     setprop gsm.version.baseband `cat /data/modem_version.txt`
 else
-    echo `strings /firmware/image/modem.b17 | grep "S86528C" | head -1` > /data/modem_version.txt
+    echo `strings /firmware/image/modem.b* | grep "QC_IMAGE_VERSION_STRING" | head -1  | sed s/QC_IMAGE_VERSION_STRING=//` > /data/modem_version.txt
     setprop gsm.version.baseband `cat /data/modem_version.txt`
 fi
