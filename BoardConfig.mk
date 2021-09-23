2bcfc8f3eec1fb420be7c9ab0d9bd7a0ad7ec588
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-24 02:44:26 +0300

    a6010: do not touch sleep disabled

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,10 +45,16 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 \
-                        androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 \
-                        androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 \
-                        loop.max_part=16 pm.sleep_mode=1 vmalloc=400M  androidboot.memcg=true
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 \
+                        androidboot.console=ttyHSL0 \
+                        androidboot.hardware=qcom \
+                        msm_rtb.filter=0x237 \
+                        ehci-hcd.park=3 \
+                        androidboot.bootdevice=7824900.sdhci \
+                        loop.max_part=16 \
+                        pm.sleep_mode=1 \
+                        vmalloc=400M \
+                        androidboot.memcg=true
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6010
