ec72f3364d5b75c6efbcb2726c7b4cfa24e8ebbc
Yaroslav Zviezda <acroreiser@gmail.com>
2021-10-08 20:49:04 +0300

    a6010: switch to Camera2

--- device.mk
+++ device.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     libshim_atomic \
     libboringssl-compat \
-    Snap
+    Camera2
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
