22bf649a1f73e193aae8e6de9d8cc05931e5bf7f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 00:46:51 +0200

    a6010: Uprev clearkey DRM HIDL to 1.4
    Change-Id: Id0895fee70495913f57bc8408ea433660467a7c7

--- device.mk
+++ device.mk
@@ -87,7 +87,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service-lazy.clearkey
+    android.hardware.drm@1.4-service-lazy.clearkey
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
