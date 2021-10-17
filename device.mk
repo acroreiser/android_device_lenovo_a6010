737153b244b019ad00c2124bedb84e44e65dfad7
Yaroslav Zviezda <acroreiser@gmail.com>
2021-10-17 20:12:52 +0300

    a6010: use regular poll state for airplane mode

--- device.mk
+++ device.mk
@@ -642,6 +642,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.apm_sim_not_pwdn=1 \
     ro.telephony.call_ring.multiple=false \
+    ro.telephony.ril.config=legacypollstate \
     ro.use_data_netmgrd=true \
     persist.radio.multisim.config=dsds \
     persist.radio.custom_ecc=1 \
