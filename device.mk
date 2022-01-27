513eefc13217c2fae97d9c1eed260b10f6a02175
Pierre-Hugues Husson <phh@phh.me>
2022-01-27 11:11:44 +0300

    a6010: Disable fdsan by default.
    This is already disabled by default on user build.
    Some HALs do not work properly with fdsan.

--- device.mk
+++ device.mk
@@ -423,6 +423,9 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.fdsan=0
+
 # Binder
 PRODUCT_PACKAGES += \
     libhwbinder \
