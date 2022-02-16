ae3147c15dcb23c6141fc4d4beaeb0bdfd2384f3
Yaroslav Zviezda <acroreiser@gmail.com>
2022-02-16 23:57:36 +0300

    a6010: set 8 background apps limit for both 1/8 & 2/16 variants

--- rootdir/etc/target_config.sh
+++ rootdir/etc/target_config.sh
@@ -6,13 +6,12 @@ ZMEM=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 if [ "$ZMEM" -gt "1100000" ]; then
 	setprop persist.device_config.runtime_native.usap_pool_enabled true
 	setprop ro.vendor.qti.sys.fw.trim_enable_memory 2147483648
-	setprop ro.vendor.qti.sys.fw.bg_apps_limit 16
 else
 	setprop persist.device_config.runtime_native.usap_pool_enabled false
 	setprop ro.vendor.qti.sys.fw.trim_enable_memory 1073741824
-	setprop ro.vendor.qti.sys.fw.bg_apps_limit 8
 fi
 
+	setprop ro.vendor.qti.sys.fw.bg_apps_limit 8
 	setprop pm.dexopt.bg-dexopt speed-profile
 	setprop pm.dexopt.install speed-profile
 	setprop dalvik.vm.usejit true
