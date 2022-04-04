d7ca4b7bafb29af7371182a51029f72206a57ea5
Yaroslav Zviezda <acroreiser@gmail.com>
2022-04-04 20:12:54 +0000

    a6010: update set_zram.sh

--- rootdir/etc/set_zram.sh
+++ rootdir/etc/set_zram.sh
@@ -1,19 +1,18 @@
 #! /vendor/bin/sh
 
-ZMEM=$(cat /proc/meminfo | grep MemTotal | awk  '{print $2}')
+TMEM=$(cat /proc/meminfo | grep MemTotal | awk  '{print $2}')
 
 # Reduce io thrashing
-if [ "$ZMEM" -gt "1100000" ]; then
+if [ "$TMEM" -gt "1100000" ]; then
 	echo "50" > /proc/sys/vm/vmpressure_level_med
-	echo 204800 > /proc/sys/vm/clean_min_kbytes
-else
-	echo 102400 > /proc/sys/vm/clean_min_kbytes
 fi
 
-
+# Reserve 10% of memory for clean pages
+let 'CMEM=(TMEM/100)*10'
+echo $CMEM > /proc/sys/vm/clean_min_kbytes
 
 # Setup ZRAM to 75% of memtotal
-let 'ZMEM=((ZMEM/100)*75)*1024'
+let 'ZMEM=((TMEM/100)*75)*1024'
 echo 1 > /sys/block/zram0/reset
 echo 'zstd' > /sys/block/zram0/comp_algorithm
 sleep 0.5
