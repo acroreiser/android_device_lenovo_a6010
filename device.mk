261e3acbdc137e82152d4d25497641532f94254b
Yaroslav Zviezda <acroreiser@gmail.com>
2022-12-22 13:42:05 +0300

    a6010: enable AutoSingleLayer
    From: https://source.android.com/docs/core/graphics/unsignaled-buffer-latch

--- device.mk
+++ device.mk
@@ -63,6 +63,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.egl.hw=1 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.auto_latch_unsignaled=true \
     debug.sf.recomputecrop=0 \
     debug.cpurend.vsync=false \
     ro.config.avoid_gfx_accel=true \
