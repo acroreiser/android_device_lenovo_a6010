74717cd9fe0af13cf8cc30643b6e4bc83481a1e9
Yaroslav Zviezda <acroreiser@gmail.com>
2023-08-09 00:19:10 +0300

    a6010: tune scheduler parameters like qcom

--- rootdir/etc/target_config.sh
+++ rootdir/etc/target_config.sh
@@ -13,15 +13,17 @@ setprop dalvik.vm.heapstartsize 4m
 # Don't account allocstalls for <= 2GB RAM targets on kernel versions < 4.9
 echo 100 > /sys/module/vmpressure/parameters/allocstall_threshold
 
-echo 1 > /proc/sys/kernel/sched_boost
 echo 50000 > /proc/sys/kernel/sched_freq_inc_notify
 echo 50000 > /proc/sys/kernel/sched_freq_dec_notify
 echo 75 > /proc/sys/kernel/sched_upmigrate
 echo 60 > /proc/sys/kernel/sched_downmigrate
 echo 3 > /proc/sys/kernel/sched_window_stats_policy
+echo 3 > /proc/sys/kernel/sched_ravg_hist_size
+
 echo 20 > /proc/sys/kernel/sched_small_task
 echo 15 > /proc/sys/kernel/sched_spill_nr_run
 echo 30 > /proc/sys/kernel/sched_mostly_idle_load
+
 echo 10 > /sys/module/process_reclaim/parameters/pressure_min
 echo 70 > /sys/module/process_reclaim/parameters/pressure_max
 
