019d4070ffc1ab37ae1806b11263c5b8a835bddd
Yaroslav Zviezda <acroreiser@gmail.com>
2023-08-15 23:16:36 +0300

    a6010: reduce UI jitter a little

--- rootdir/etc/target_config.sh
+++ rootdir/etc/target_config.sh
@@ -41,3 +41,7 @@ sysctl -w vm.memfd_noexec=1
 
 # Tune task scheduler to prefer idle cpus to place task
 sysctl -w kernel.sched_prefer_idle=1
+
+# Tune task scheduler to reduce possibility to pick some heavy task
+# to the same cpu with ui-threads
+sysctl -w kernel.sched_spill_load=85
