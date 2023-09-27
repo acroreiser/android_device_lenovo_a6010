83eedbd2c994a1e066dfc5d1adafd2d47233e6d6
iusmac <iusico.maxim@libero.it>
2023-09-27 21:23:46 +0300

    a6010: Add HW_TIMEOUT_MULTIPLIER prop
    Increase watchdog timeout multiplier to workaround ANRs on A13 due to Magisk.
    
    5 * 6 = 30 secs.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -629,7 +629,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
     ro.config.max_starting_bg=1 \
     ro.am.no_kill_cached_processes_until_boot_completed=false \
-    ro.am.no_kill_cached_processes_post_boot_completed_duration_millis=0
+    ro.am.no_kill_cached_processes_post_boot_completed_duration_millis=0 \
+    ro.hw_timeout_multiplier=6
 
 # Dexopt
 PRODUCT_PROPERTY_OVERRIDES += \
