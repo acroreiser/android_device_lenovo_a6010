1535e88219ecbd0c7e1d7646d2a61eea150bb93c
Yaroslav Zviezda <acroreiser@gmail.com>
2023-10-24 23:02:42 +0300

    a6010: set_tasks: move hwcomposer and msm_watchdog to cpusets

--- rootdir/etc/set_tasks.sh
+++ rootdir/etc/set_tasks.sh
@@ -3,11 +3,9 @@
 # move cpu-hungry kswapd and mmcqd/0 tasks to background cpuset
 echo $(pgrep kswapd) > /dev/cpuset/background/tasks
 echo $(pgrep mmcqd/0 | head -n 1) > /dev/cpuset/background/tasks
+echo $(pgrep msm_watchdog) > /dev/cpuset/background/tasks
 
-# move UI-related threads to top-app cpuset
+# move critical and UI-related tasks to top-app cpuset
+echo $(pgrep composer) > /dev/cpuset/top-app/tasks
 echo $(pgrep mdss_dsi_event) > /dev/cpuset/top-app/tasks
 echo $(pgrep kgsl_worker_thr) > /dev/cpuset/top-app/tasks
-
-# move watchdog thread to foregrond cpuset
-echo $(pgrep msm_watchdog) > /dev/cpuset/foreground/tasks
-
