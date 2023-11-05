ea54f1229f1c918e9a3275b3353dd194c9cb09cb
Yaroslav Zviezda <acroreiser@gmail.com>
2023-11-05 20:29:14 +0300

    a6010: set_tasks: move more daemons to background cpuset

--- rootdir/etc/set_tasks.sh
+++ rootdir/etc/set_tasks.sh
@@ -4,6 +4,13 @@
 echo $(pgrep kswapd) > /dev/cpuset/background/tasks
 echo $(pgrep mmcqd/0 | head -n 1) > /dev/cpuset/background/tasks
 echo $(pgrep msm_watchdog) > /dev/cpuset/background/tasks
+echo $(pgrep irqbalance) > /dev/cpuset/background/tasks
+echo $(pgrep rild | head -n 1) > /dev/cpuset/background/tasks
+echo $(pgrep rild | head -n 2 | sed /$(head -n1)/d) > /dev/cpuset/background/tasks
+echo $(pgrep qmuxd) > /dev/cpuset/background/tasks
+echo $(pgrep netmgrd) > /dev/cpuset/background/tasks
+echo $(pgrep logd) > /dev/cpuset/background/tasks
+echo $(pgrep wcnss_service) > /dev/cpuset/background/tasks
 
 # move critical and UI-related tasks to top-app cpuset
 echo $(pgrep composer) > /dev/cpuset/top-app/tasks
