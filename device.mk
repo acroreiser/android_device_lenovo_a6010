fa8680658c58b51b3fd0e0715c09b5b58aa07d82
Yaroslav Zviezda <acroreiser@gmail.com>
2023-11-15 00:27:25 +0300

    a6010: do not set ro.vndk.version
    Build system will set ro.vndk.version=34.

--- device.mk
+++ device.mk
@@ -431,8 +431,7 @@ PRODUCT_COPY_FILES += \
 
 # First api level, device has been commercially launched
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=21 \
-    ro.vndk.version=current
+    ro.product.first_api_level=21
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
