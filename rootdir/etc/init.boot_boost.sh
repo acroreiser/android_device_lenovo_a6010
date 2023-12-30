1c308b5d55a211113b679bf0fad0286109c5a166
Yaroslav Zviezda <acroreiser@gmail.com>
2023-12-31 01:07:00 +0300

    a6010: kill boot-boost
    1. Drop boot-boost logic
    2. Switch to recently backported schedutil governor
    3. Tune schedutil for our needs

--- rootdir/etc/init.boot_boost.sh
+++ rootdir/etc/init.boot_boost.sh
@@ -1,20 +1,7 @@
 #!/system/vendor/bin/sh
 #
-# Script to boost cpus and gpus (sd-410) for 40 sec after boot, to
-# reduce lag and bad ux that lasts for about 1 min after booting
+# Script to setup cpus (sd-410) after boot
 
-echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "performance" > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/governor
-echo 400000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
-
-sleep 40
-
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_migration_notif
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/fast_ramp_down
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/ignore_hispeed_on_notif
-
-echo "msm-adreno-tz" > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/governor
-echo 200000000 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/min_freq
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 5000 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+echo 90 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
