7b45eb1182ce299b17f7b569271225753dcb5574
Yaroslav Zviezda <acroreiser@gmail.com>
2024-07-09 22:15:40 +0300

    a60x0: make lowmemorykiller more tolerant to swap usage and delays
    This will increase multitasking.

--- device.mk
+++ device.mk
@@ -116,8 +116,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1 \
     ro.config.per_app_memcg=false \
     ro.lmk.psi_complete_stall_ms=400 \
-    ro.lmk.psi_partial_stall_ms=100 \
-    ro.lmk.swap_free_low_percentage=35 \
     ro.lmk.filecache_min_kb=110592
 
 # Avoid thrashing to prevent freezes.
