a20c320aa78ad353b9f3b60db6506b433dd92949
Yaroslav Zviezda <acroreiser@gmail.com>
2024-10-30 23:08:05 +0300

    a6010: drop additional resource limiting for background and dex2oat cgroups

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -27,13 +27,6 @@ sysctl -w kernel.sched_spill_load=100
 echo 1 > /sys/devices/platform/kcal_ctrl.0/kcal_enable
 echo 261 > /sys/devices/platform/kcal_ctrl.0/kcal_sat
 
-# Limit CPU and IO resources for background tasks
-echo 102 > /dev/cpuctl/background/cpu.shares
-echo 100 > /dev/blkio/background/blkio.weight
-
-# Limit resources for dex2oat
-echo 102 > /dev/cpuctl/dex2oat/cpu.shares
-
 # Disable wsf for all targets beacause we are using efk.
 # wsf Range : 1..1000 So set to bare minimum value 1.
 echo 1 > /proc/sys/vm/watermark_scale_factor
