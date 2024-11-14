8d9af15e80b01d9b6044dce3ad29d8346d74941a
Yaroslav Zviezda <acroreiser@gmail.com>
2024-11-15 01:08:34 +0300

    Revert "a6010: Disable codec2"
    This reverts commit 6b8c592ff3b467904e21457a3bcb2275f3c43832.

--- device.mk
+++ device.mk
@@ -528,7 +528,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank=0 \
     vendor.mediacodec.binder.size=4 \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
-    debug.stagefright.ccodec=0 \
     vidc.enc.narrow.searchrange=1 \
     ro.media.maxmem=268435456
 
