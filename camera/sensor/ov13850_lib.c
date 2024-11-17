b8a7a67b66f608d42f9957fbda5361e8cacfcabe
Yaroslav Zviezda <acroreiser@gmail.com>
2024-11-17 21:54:55 +0300

    a6010: camera: ov13850: reduce max ISO to 1600

--- camera/sensor/ov13850_lib.c
+++ camera/sensor/ov13850_lib.c
@@ -148,7 +148,7 @@ static sensor_manual_exposure_info_t manual_exp_info = {
   .min_exposure_time = 100000,/*in nano sec = 1line*/
   .max_exposure_time = 1000000000,/*in nano sec = FFFF lines*/
   .min_iso = 100,
-  .max_iso = 3200,
+  .max_iso = 1600,
 };
 
 static struct msm_sensor_exp_gain_info_t exp_gain_info = {
