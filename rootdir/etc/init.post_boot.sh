fce0bca899b8acbc54cdfb490a18c21132fdb9a5
Yaroslav Zviezda <acroreiser@gmail.com>
2024-11-17 22:34:27 +0300

    a6010: reduce kswapd threads to 2

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -36,7 +36,7 @@ echo 10800 > /proc/sys/vm/extra_free_kbytes
 echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
 
 # Set kswapd threads
-echo 4 > /proc/sys/vm/kswapd_threads
+echo 2 > /proc/sys/vm/kswapd_threads
 
 echo 5430 > /proc/sys/vm/min_free_kbytes
 
