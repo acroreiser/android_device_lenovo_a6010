d5f1c4600b25e90d3500c35eb4a0c4864519f3cc
Yaroslav Zviezda <acroreiser@gmail.com>
2024-11-18 22:25:49 +0300

    a6010: throttle background tasks IO bandwidth to 1MB/s for read and 0.5MB/s for write

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -45,3 +45,9 @@ echo 1 > /proc/sys/kernel/sched_rr_timeslice_ms
 
 # Set KGSL pwrlevel
 echo 2 > /sys/class/kgsl/kgsl-3d0/default_pwrlevel
+
+# Limit background tasks IO bandwidth
+echo "179:0 1048576" > /dev/blkio/background/blkio.throttle.read_bps_device
+echo "179:0 524288" > /dev/blkio/background/blkio.throttle.write_bps_device
+echo "179:64 1048576" > /dev/blkio/background/blkio.throttle.read_bps_device
+echo "179:64 524288" > /dev/blkio/background/blkio.throttle.write_bps_device
