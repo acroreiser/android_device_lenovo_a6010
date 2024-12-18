9a4f0febdbe341fa78d12c7e9a779077a04c0f2c
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-18 22:34:57 +0300

    a6010: limit cputime for dex2oat to 200% (2 cpus)

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -57,3 +57,7 @@ echo 2000000 > /dev/cpuctl/background/cpu.cfs_quota_us
 # Limit cputime for foreground tasks
 echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
 echo 3000000 > /dev/cpuctl/background/cpu.cfs_quota_us
+
+# Limit cputime for dex2oat
+echo 1000000 > /dev/cpuctl/dex2oat/cpu.cfs_period_us
+echo 2000000 > /dev/cpuctl/dex2oat/cpu.cfs_quota_us
