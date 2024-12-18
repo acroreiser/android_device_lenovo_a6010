d03b1f5ebcbff5ac7cbc3c660cb83252268340f6
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-18 22:33:06 +0300

    a6010: limit cputime for background apps to 200% (2 cpus)

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -49,3 +49,7 @@ echo "179:0 8388608" > /dev/blkio/background/blkio.throttle.read_bps_device
 echo "179:0 4194304" > /dev/blkio/background/blkio.throttle.write_bps_device
 echo "179:64 8388608" > /dev/blkio/background/blkio.throttle.read_bps_device
 echo "179:64 4194304" > /dev/blkio/background/blkio.throttle.write_bps_device
+
+# Limit cputime for background tasks
+echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
+echo 2000000 > /dev/cpuctl/background/cpu.cfs_quota_us
