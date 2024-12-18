f49b20c5b4e0454d0a9080bcdb0a636337d070a3
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-18 22:36:08 +0300

    a6010: do not reserve additional memory for admin and user

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -31,6 +31,8 @@ echo 261 > /sys/devices/platform/kcal_ctrl.0/kcal_sat
 echo 0 > /proc/sys/vm/extra_free_kbytes
 echo 230 > /proc/sys/vm/watermark_scale_factor
 echo 10572 > /proc/sys/vm/min_free_kbytes
+echo 0 > /proc/sys/vm/admin_reserve_kbytes
+echo 0 > /proc/sys/vm/user_reserve_kbytes
 
 # Set allocstall_threshold to 0
 echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
