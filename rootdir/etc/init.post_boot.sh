f4e5440ed0f3a77574ca1fcb3809c076794ef9e7
Rick Yiu <rickyiu@google.com>
2024-12-23 21:29:34 +0300

    a6010: Setup cpu.shares to throttle background groups

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -59,3 +59,14 @@ echo 400000 > /dev/cpuctl/background/cpu.cfs_quota_us
 # Limit cputime for dex2oat
 echo 1000000 > /dev/cpuctl/dex2oat/cpu.cfs_period_us
 echo 400000 > /dev/cpuctl/dex2oat/cpu.cfs_quota_us
+
+# Setup cpu.shares to throttle background groups (bg ~ 5% sysbg ~ 5% dex2oat ~2.5%)
+echo 1024 > /dev/cpuctl/background/cpu.shares
+echo 1024 > /dev/cpuctl/system-background/cpu.shares
+echo 512 > /dev/cpuctl/dex2oat/cpu.shares
+echo 20480 > /dev/cpuctl/foreground/cpu.shares
+echo 20480 > /dev/cpuctl/top-app/cpu.shares
+echo 20480 > /dev/cpuctl/camera-daemon/cpu.shares
+echo 20480 > /dev/cpuctl/system/cpu.shares
+echo 20480 > /dev/cpuctl/nnapi-hal/cpu.shares
+echo 20480 > /dev/cpuctl/rt/cpu.shares
