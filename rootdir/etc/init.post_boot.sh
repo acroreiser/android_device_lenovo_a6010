8ca223e80be575fbcc3b9c1c4b69f8504905ca30
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-23 21:24:52 +0300

    a6010: harden cputime limits for background and dex2oat

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -54,8 +54,8 @@ echo "179:64 8388608" > /dev/blkio/background/blkio.throttle.write_bps_device
 
 # Limit cputime for background tasks
 echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
-echo 2000000 > /dev/cpuctl/background/cpu.cfs_quota_us
+echo 400000 > /dev/cpuctl/background/cpu.cfs_quota_us
 
 # Limit cputime for dex2oat
 echo 1000000 > /dev/cpuctl/dex2oat/cpu.cfs_period_us
-echo 2000000 > /dev/cpuctl/dex2oat/cpu.cfs_quota_us
+echo 400000 > /dev/cpuctl/dex2oat/cpu.cfs_quota_us
