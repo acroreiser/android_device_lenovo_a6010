ce2b3a8c2d1420dd7f37a736808ba518c55261b8
Yaroslav Zviezda <acroreiser@gmail.com>
2025-01-23 21:18:42 +0200

    a60x0: include a6010/a6000 specific blobs

--- lineage_a6000.mk
+++ lineage_a6000.mk
@@ -78,4 +78,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/QRD/QRD_Headset_cal.acdb
 
 $(call inherit-product, vendor/lenovo/a6010/a6010-vendor.mk)
+$(call inherit-product, vendor/lenovo/a6010/a60x0.mk)
 $(call inherit-product, vendor/lenovo/a6010/a6000.mk)
