40e69fd4fc63e2d4f336d6a85bdfc1c8bf3c084b
Yaroslav Zviezda <acroreiser@gmail.com>
2025-01-24 22:45:11 +0200

    sisleyr: add GMS product variant

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,7 +2,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_a6010.mk \
     $(LOCAL_DIR)/lineage_a6010_gms_go_2gb.mk \
     $(LOCAL_DIR)/lineage_a6000.mk \
-    $(LOCAL_DIR)/lineage_sisleyr.mk
+    $(LOCAL_DIR)/lineage_sisleyr.mk \
+    $(LOCAL_DIR)/lineage_sisleyr_gms_go_2gb.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_a6010-eng \
@@ -14,4 +15,5 @@ COMMON_LUNCH_CHOICES := \
     lineage_a6000-userdebug \
     lineage_a6000-user \
     lineage_sisleyr-eng \
-    lineage_sisleyr-user
+    lineage_sisleyr-user \
+    lineage_sisleyr_gms_go_2gb-user
