dd6566f6e671429dcce044406cd979f0c6f4856e
Yaroslav Zviezda <acroreiser@gmail.com>
2025-04-21 19:43:35 +0300

    msm8916: do not limit background io bandwidth

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -43,12 +43,6 @@ echo 1 > /proc/sys/vm/kswapd_threads
 # Set KGSL pwrlevel
 echo 2 > /sys/class/kgsl/kgsl-3d0/default_pwrlevel
 
-# Limit background tasks IO bandwidth
-echo "179:0 8388608" > /dev/blkio/background/blkio.throttle.read_bps_device
-echo "179:0 8388608" > /dev/blkio/background/blkio.throttle.write_bps_device
-echo "179:64 8388608" > /dev/blkio/background/blkio.throttle.read_bps_device
-echo "179:64 8388608" > /dev/blkio/background/blkio.throttle.write_bps_device
-
 # Limit cputime for background tasks
 echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
 echo 2000000 > /dev/cpuctl/background/cpu.cfs_quota_us
