dc57df534ac6ccdd8b557b8a5a578e7fcfc81de3
Yaroslav Zviezda <acroreiser@gmail.com>
2025-05-25 21:50:12 +0300

    msm8916: set_zram: switch to lz4kd

--- rootdir/etc/set_zram.sh
+++ rootdir/etc/set_zram.sh
@@ -5,7 +5,7 @@ TMEM=$(cat /proc/meminfo | grep MemTotal | awk  '{print $2}')
 # Setup ZRAM to 75% of memtotal
 let 'ZMEM=((TMEM/100)*75)*1024'
 echo 1 > /sys/block/zram0/reset
-echo 'lz4' > /sys/block/zram0/comp_algorithm
+echo 'lz4kd' > /sys/block/zram0/comp_algorithm
 echo $ZMEM > /sys/block/zram0/disksize
 mkswap /dev/block/zram0
 swapon -d /dev/block/zram0
