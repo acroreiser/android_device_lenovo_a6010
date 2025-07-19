15412c5d043828f8cd723bcac0608260eeed4249
Bruno Martins <bgcngm@gmail.com>
2025-07-19 12:08:51 +0300

    msm8916: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.4-service-lazy.clearkey
+    android.hardware.drm-service-lazy.clearkey
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
