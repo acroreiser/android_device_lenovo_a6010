96ff1769e32fce73f3cccfa1c51a9c575d908686
Yaroslav Zviezda <acroreiser@gmail.com>
2025-07-25 00:37:18 +0300

    msm8916: switch to our in-tree radio 1.4 wrapper

--- device.mk
+++ device.mk
@@ -491,7 +491,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0-service \
-    android.hardware.radio@1.4-service.legacy
+    android.hardware.radio@1.4-service.msm8916
 
 # Configuring device during boot
 PRODUCT_PACKAGES += \
