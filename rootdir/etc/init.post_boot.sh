abd69922b030b25c018695c6382ea033c2692e89
Yaroslav Zviezda <acroreiser@gmail.com>
2026-03-07 12:57:28 -0500

    msm8916: do not slow down cpu too fast
    Improves UI jitter.
    
    Test: uibench

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -5,7 +5,7 @@
 # Setup CPUFreq
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
-echo 300 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+echo 500 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
 echo 99 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
 echo 1209600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
 
