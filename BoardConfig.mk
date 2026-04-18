a401828afb7f9836c248ad7c3dc7f0339369a51e
LuK1337 <priv.luk@gmail.com>
2026-04-18 21:48:20 +0300

    a6010: Set PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,8 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
