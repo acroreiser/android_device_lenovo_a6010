243dab9c52a9054d9450d3555eb584ee5121b73a
Yaroslav Zviezda <acroreiser@gmail.com>
2026-04-18 21:29:43 +0300

    a6010: add_lunch_combo is deprecated

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_a6010.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a6010-eng
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo omni_a6010-eng
-add_lunch_combo omni_a6010-userdebug
