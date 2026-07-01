3d0de5d09a4ddd2529aefcf13768501deb1fef5e
Yaroslav Zviezda <acroreiser@gmail.com>
2026-07-01 15:37:32 -0400

    msm8916: schedutil: jump to hispeed freq at 80% utilization

--- rootdir/etc/init.post_boot.sh
+++ rootdir/etc/init.post_boot.sh
@@ -6,7 +6,7 @@
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
 echo 500 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
-echo 99 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
+echo 80 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
 echo 1209600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
 
 # Setup CPU-boost
